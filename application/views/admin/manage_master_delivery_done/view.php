a7fc273e953b3686fae196306d0329f184913b93
Kapil <kapil@Kapils-MacBook-Air.local>
2024-02-14 12:00:59 +0530

    add

--- application/views/admin/manage_master_delivery_done/view.php
+++ application/views/admin/manage_master_delivery_done/view.php
@@ -6,6 +6,19 @@
             </button>
         </a> */ ?>
    	</div>
+	   <form method="get">
+		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+			<div class="form-group nk-datapk-ctm form-elet-mg" id="data_1">
+				<div class="input-group date nk-int-st">
+					<label>Select Date</label>
+					<input type="date" class="form-control" value="<?= $vdt1; ?>" name="vdt">
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 text-right">
+			<button type="submit" name="submit" class="btn btn-success notika-btn-success waves-effect" value="submit">Submit</button>
+		</div>
+	</form>
 	<div class="col-xs-12">
         <div class="table-responsive">
 			<table class="table table-striped table-bordered table-hover" id="example-table">
