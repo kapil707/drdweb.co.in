8a6f9dac35364d487c651fcd48204523951c5b84
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-06 17:14:38 +0530

    add

--- application/views/admin/manage_master_tracking/view2.php
+++ application/views/admin/manage_master_tracking/view2.php
@@ -8,7 +8,7 @@
    	</div>
 	<div class="col-xs-12">
     
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBHDSrVBSnNmTVBCYSPYqdz8qSKmG47Yxk"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBHDSrVBSnNmTVBCYSPYqdz8qSKmG47Yxk&callback=initMap" async defer></script>
 
   <div id="map" style="height: 400px;"></div>afdasfd
   <script>
