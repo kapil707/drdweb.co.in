a42b6b602a40121aec600701ad26ce6845a18f10
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-26 16:33:42 +0530

    add

--- application/views/sms.php
+++ application/views/sms.php
@@ -1,6 +1,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/1.11.6/css/jquery.dataTables.min.css">
+<link href="<?= base_url()?>/assets/css/plugins/dataTables/datatables.min.css" rel="stylesheet">
 <div class="container pt-5">
 	<div class="row">
         <div class="col-md-3">
@@ -37,10 +37,11 @@
 		</div>
 	</div>
 </div>
+<script src="<?= base_url()?>/assets/js/jquery-3.1.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery"></script>
 <script src="https://cdn.jsdelivr.net/npm/moment"></script>
 <script src="https://cdn.jsdelivr.net/npm/daterangepicker"></script>
-<script src="https://cdn.datatables.net/1.11.6/js/jquery.dataTables.min.js"></script>
+<script src="<?= base_url()?>/assets/js/plugins/dataTables/datatables.min.js"></script>
 <!-- Page-Level Scripts -->
 <script>
 $(document).ready(function(){
