2ab4881cc80091a0c797a0f120894ec0329d830e
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-26 17:07:57 +0530

    add

--- application/views/sms.php
+++ application/views/sms.php
@@ -41,7 +41,10 @@
 </div>
 <script src="<?= base_url()?>/assets/js/jquery-3.1.1.min.js"></script>
 <script src="<?= base_url()?>/assets/js/plugins/dataTables/datatables.min.js"></script>
-<script src="<?= base_url()?>/assets/js/plugins/daterangepicker/daterangepicker.js"></script>
+
+<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 <script>
 $(document).ready(function(){
 
