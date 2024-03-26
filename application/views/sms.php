2deb2d75a26281526f28cb67333d44d0d4109dd2
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-26 15:47:49 +0530

    add

--- application/views/sms.php
+++ application/views/sms.php
@@ -60,30 +60,30 @@ $(document).ready(function(){
 			}
 		}
 	});
-	
-	function load_datatable(mydata){
-		$('#example-table').DataTable({
-			data: mydata,
-			pageLength: 25,
-			responsive: true,
-			dom: '<"html5buttons"B>lTfgitp',
-			buttons: [
-				{extend: 'copy'},
-				{extend: 'csv'},
-				{extend: 'excel', title: 'ExampleFile'},
-				{extend: 'pdf', title: 'ExampleFile'},
-				{extend: 'print',
-					customize: function (win){
-						$(win.document.body).addClass('white-bg');
-						$(win.document.body).css('font-size', '10px');
-						$(win.document.body).find('table')
-								.addClass('compact')
-								.css('font-size', 'inherit');
-				}
-				}
-			]
-		});
-	}
 });
+
+function load_datatable(mydata){
+	$('#example-table').DataTable({
+		data: mydata,
+		pageLength: 25,
+		responsive: true,
+		dom: '<"html5buttons"B>lTfgitp',
+		buttons: [
+			{extend: 'copy'},
+			{extend: 'csv'},
+			{extend: 'excel', title: 'ExampleFile'},
+			{extend: 'pdf', title: 'ExampleFile'},
+			{extend: 'print',
+				customize: function (win){
+					$(win.document.body).addClass('white-bg');
+					$(win.document.body).css('font-size', '10px');
+					$(win.document.body).find('table')
+							.addClass('compact')
+							.css('font-size', 'inherit');
+			}
+			}
+		]
+	});
+}
 </script>
 <script src="https://cdn.datatables.net/scroller/2.2.0/js/dataTables.scroller.min.js"></script>
