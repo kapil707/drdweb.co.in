753e978dc5be0abea60e5cf15c9cd71de5b8cc37
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-26 16:48:50 +0530

    add

--- application/views/sms.php
+++ application/views/sms.php
@@ -38,11 +38,11 @@
 		</div>
 	</div>
 </div>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 
 
 <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 <script src="https://cdn.datatables.net/2.0.3/js/dataTables.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 <!-- Page-Level Scripts -->
 <script>
 $(document).ready(function(){
