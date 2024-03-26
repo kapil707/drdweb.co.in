672eb08f62dc720970406eb1249cc6279e9f99ae
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-26 16:16:03 +0530

    add

--- application/views/sms.php
+++ application/views/sms.php
@@ -1,22 +1,19 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link href="https://cdn.datatables.net/2.0.3/css/dataTables.dataTables.css" rel="stylesheet">
-<div class="container">
+<div class="container pt-5">
 	<div class="row">
         <div class="col-md-3">
-            <label for="start-date">Start Date:</label>
-            <input type="date" id="start-date" class="form-control">
+            <input type="date" id="start-date" class="form-control" placeholder="Start Date:">
         </div>
         <div class="col-md-3">
-            <label for="end-date">End Date:</label>
-            <input type="date" id="end-date" class="form-control">
+            <input type="date" id="end-date" class="form-control"  placeholder="End Date:">
         </div>
         <div class="col-md-2">
-            <label>&nbsp;</label>
             <button id="filter-btn" class="btn btn-primary btn-block">Filter</button>
         </div>
     </div>
 	<div class="row">
-		<div class="col-sm-12  p-5">
+		<div class="col-sm-12  pt-1 pb-5">
 			<table class="table table-striped table-bordered table-hover dataTables-example" id="example-table">
 				<thead>
 					<tr>
