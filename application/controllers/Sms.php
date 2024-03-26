c2b8e8eea9fa04b1f56258ca8137c3b713a66795
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-26 15:30:16 +0530

    add

--- application/controllers/Sms.php
+++ application/controllers/Sms.php
@@ -1,8 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Sms extends CI_Controller {		
-	public function index()
-	{	
+	public function index()	{	
 		$this->load->view('sms');
 	}
 }
---
+++ application/views/sms.php
@@ -0,0 +1,28 @@
+<link href="<?= base_url()?>/assets/css/plugins/dataTables/datatables.min.css" rel="stylesheet">
+adfsaf
+<script src="<?= base_url()?>/assets/js/plugins/dataTables/datatables.min.js"></script>
+<!-- Page-Level Scripts -->
+<script>
+	$(document).ready(function(){
+		$('.dataTables-example').DataTable({
+			pageLength: 25,
+			responsive: true,
+			dom: '<"html5buttons"B>lTfgitp',
+			buttons: [
+				{ extend: 'copy'},
+				{extend: 'csv'},
+				{extend: 'excel', title: 'ExampleFile'},
+				{extend: 'pdf', title: 'ExampleFile'},
+				{extend: 'print',
+					customize: function (win){
+						$(win.document.body).addClass('white-bg');
+						$(win.document.body).css('font-size', '10px');
+						$(win.document.body).find('table')
+								.addClass('compact')
+								.css('font-size', 'inherit');
+				}
+				}
+			]
+		});
+	});
+</script>
