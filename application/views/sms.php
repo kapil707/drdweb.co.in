2a6dcf038cbde46eecece5362ea59985511246d5
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-26 16:09:39 +0530

    add

--- application/views/sms.php
+++ application/views/sms.php
@@ -1,6 +1,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link href="https://cdn.datatables.net/2.0.3/css/dataTables.dataTables.css" rel="stylesheet">
-<div class="container-lg">
+<div class="container-xl">
 	<div class="row">
 		<div class="col-sm-12  pt-5">
 			<table class="table table-striped table-bordered table-hover dataTables-example" id="example-table">
