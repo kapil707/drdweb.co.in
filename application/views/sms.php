0ff6c9c8e2bcecd44f54109d260f961ebb411996
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-26 15:38:22 +0530

    add

--- application/views/sms.php
+++ application/views/sms.php
@@ -1,15 +1,73 @@
 <link href="<?= base_url()?>/assets/css/plugins/dataTables/datatables.min.css" rel="stylesheet">
-adfsaf
+<table class="table table-striped table-bordered table-hover dataTables-example">
+	<thead>
+		<tr>
+			<th>
+				Sno.
+			</th>
+			<th>
+				Sender
+			</th>
+			<th>
+				Message
+			</th>
+			<th>
+				Date
+			</th>
+			<th>
+				Time
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+	
+	</tbody>
+</table>
+<script src="<?= base_url()?>/assets/js/jquery-3.1.1.min.js"></script>
 <script src="<?= base_url()?>/assets/js/plugins/dataTables/datatables.min.js"></script>
 <!-- Page-Level Scripts -->
 <script>
-	$(document).ready(function(){
-		$('.dataTables-example').DataTable({
+$(document).ready(function(){
+
+	$from_date = "2024-03-22";
+	$from_date = "2024-03-24";
+
+	let mydata = [];
+	$.ajax({
+		type       : "POST",
+		data       : {from_date:from_date,to_date:to_date} ,
+		url        : "<?php echo base_url(); ?>upload_sms/api01/get_upload_sms",
+		cache	   : false,
+		error: function(){
+		},
+		success    : function(data){
+			if(data.items=="")
+			{
+				$.each(data.items, function(i,item){
+					if (item){
+						id 			= item.id;
+						sender 		= item.sender;
+						message_body= item.message_body;
+						date 		= item.date;
+						time 		= item.time;
+						datetime 	= item.datetime;
+
+						mydata.push(id,sender,message_body,date,time);
+					}
+				});
+				load_datatable()
+			}
+		}
+	});
+	
+	function load_datatable(){
+		$('#example-table').DataTable({
+			data: mydata,
 			pageLength: 25,
 			responsive: true,
 			dom: '<"html5buttons"B>lTfgitp',
 			buttons: [
-				{ extend: 'copy'},
+				{extend: 'copy'},
 				{extend: 'csv'},
 				{extend: 'excel', title: 'ExampleFile'},
 				{extend: 'pdf', title: 'ExampleFile'},
@@ -24,5 +82,7 @@ adfsaf
 				}
 			]
 		});
-	});
+	}
+});
 </script>
+<script src="https://cdn.datatables.net/scroller/2.2.0/js/dataTables.scroller.min.js"></script>
