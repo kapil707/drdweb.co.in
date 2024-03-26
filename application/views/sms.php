f96232b25d4a2d9820f34da6d7f140eb5fa4a20b
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-03-26 17:01:22 +0530

    add

--- application/views/sms.php
+++ application/views/sms.php
@@ -39,8 +39,8 @@
 		</div>
 	</div>
 </div>
-<script src="<?= base_url()?>/assets/js/jquery-3.1.1.min.js"></script>
-<script src="<?= base_url()?>/assets/js/plugins/dataTables/datatables.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+<script src="https://cdn.datatables.net/2.0.3/js/dataTables.js"></script>
 
 <script>
 $(document).ready(function(){
