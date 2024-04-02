40337735e7a6d8d37c11daef328e41c08e6b8e3c
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-04-02 12:48:05 +0530

    add

--- application/controllers/Sms.php
+++ application/controllers/Sms.php
@@ -4,4 +4,16 @@ class Sms extends CI_Controller {
 	public function index()	{	
 		$this->load->view('sms');
 	}
+
+	public function split_function(){
+		
+		$from_date = '2024-04-02';
+		$to_date = '2024-04-02';
+
+		$result = $this->db->query("select * from tbl_upload_sms where date BETWEEN '$from_date' AND '$to_date' limit 100")->result();
+		foreach($result as $row){
+			echo $row;
+			die();
+		}
+	}
 }
