41fa6dfc70607b2148b8f773a6cfcd353a60774a
kapil-asus-new <kapil707sharma@gmail.com>
2024-04-10 13:34:54 +0530

    add

---
+++ application/controllers/cronjob-drdweb/Cronjob_master.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Cronjob_admin_report extends CI_Controller 
+{
+	public function __construct(){
+
+		parent::__construct();
+	}
+	
+	public function get_master_exe_call()
+	{
+		$db_master = $this->load->database('db_master', TRUE);
+
+		$date = date("Y-m-d");
+		$time = date("h:i a");
+		$current_time = date("H:i:s");
+		$time = date("H:i", strtotime($current_time . ' - 1 minute'));
+		$result = $db_master("select * from tbl_cronjob_time_for_exe where time<='$time'")->result();
+		foreach($result as $row){
+			echo $row->time;
+			echo "<br>";
+		}
+	}
+}
