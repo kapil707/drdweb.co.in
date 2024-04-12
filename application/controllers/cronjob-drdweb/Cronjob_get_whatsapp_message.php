67384744b28b4f4b93654717db39491a916572d8
kapil707 <kapilsharma@kapils-MacBook-Air.local>
2024-04-12 10:23:43 +0530

    add

---
+++ application/controllers/cronjob-drdweb/Cronjob_get_whatsapp_message.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Cronjob_get_whatsapp_message extends CI_Controller 
+{
+	public function __construct(){
+
+		parent::__construct();
+	}
+	
+	public function get_whatsapp_message()
+	{
+	
+		$parmiter = '';
+		$curl = curl_init();
+		
+		curl_setopt_array(
+			$curl,
+			array(
+				CURLOPT_URL =>"http://97.74.82.55:5000/messages?from=07/04/2024&to=07/04/2024",
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => '',
+				CURLOPT_MAXREDIRS => 0,
+				CURLOPT_TIMEOUT => 300,
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => 'POST',
+				CURLOPT_POSTFIELDS => $parmiter,
+				CURLOPT_HTTPHEADER => array(
+					'Content-Type: application/json',
+				),
+			)
+		);
+
+		$response = curl_exec($curl);
+		print_r($response);
+		curl_close($curl);
+	}
+}
