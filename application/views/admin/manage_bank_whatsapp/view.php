4dd647c9063d98b990e07bd9fcb0ffc4c6d9afda
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-04-15 10:01:26 +0530

    add

--- application/views/admin/manage_bank_whatsapp/view.php
+++ application/views/admin/manage_bank_whatsapp/view.php
@@ -6,6 +6,32 @@
             </button>
         </a> -->
    	</div>
+	<?php $curl = curl_init();
+		
+		curl_setopt_array(
+			$curl,
+			array(
+				CURLOPT_URL =>"http://97.74.82.55:5000/messages?from=07/04/2024&to=07/04/2024",
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => '',
+				CURLOPT_MAXREDIRS => 0,
+				CURLOPT_TIMEOUT => 300,
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => 'GET',
+				CURLOPT_POSTFIELDS => $parmiter,
+				CURLOPT_HTTPHEADER => array(
+					'Content-Type: application/json',
+				),
+			)
+		);
+
+		$response = curl_exec($curl);
+		//print_r($response);
+		curl_close($curl);
+
+		$data1 = json_decode($response, true); // Convert JSON string to associative array
+		?>
 	<div class="col-xs-12">
         <div class="table-responsive">
 			<table class="table table-striped table-bordered table-hover" id="example-table">
@@ -38,7 +64,7 @@
                     </tr>
                 </thead>
 				<tbody>
-					<?php
+					<?php /*
 					foreach($result as $row){
 						?>
 						<tr><?php $body = utf8_decode($row->body); ?>
@@ -52,6 +78,37 @@
 							<td><?php echo $row->timestamp; ?></td>
 						</tr>
 						<?php
+					}
+					 */?>
+					 <?php
+					if (isset($data['messages'])) {
+						foreach ($data['messages'] as $message) {
+							$body = isset($message['body']) ? $message['body'] : "Body not found";
+
+							$date = isset($message['date']) ? $message['date'] : "Date not found";
+
+							$extracted_text = isset($message['extracted_text']) ? $message['extracted_text'] : "extracted_text not found";
+
+							$from_number = isset($message['from_number']) ? $message['from_number'] : "Date not found";
+							
+							$id = isset($message['id']) ? $message['from_number'] : "id not found";
+
+							$screenshot_image = isset($message['screenshot_image']) ? $message['screenshot_image'] : "screenshot_image not found";
+
+							$timestamp = isset($message['timestamp']) ? $message['timestamp'] : "timestamp not found";
+							?>
+							<tr>
+								<td><?php echo $body; ?></td>
+								<td><?php echo $date; ?></td>
+								<td><?php echo $extracted_text; ?></td>
+								<td><?php echo $from_number; ?></td>
+								<td><?php echo $message_id; ?></td>
+								<td><?php echo $screenshot_image; ?></td>
+								<td><?php echo $timestamp; ?></td>
+								<td><?php echo $timestamp; ?></td>
+							</tr>
+							<?php
+						}
 					}
 					?>
 				</tbody>
