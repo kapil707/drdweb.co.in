ee05deb9c3a000bd8493e85e23ded6236911549a
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-04-18 15:11:59 +0530

    add

--- application/views/admin/manage_bank/add.php
+++ application/views/admin/manage_bank/add.php
@@ -107,7 +107,7 @@
 		$start_row = "13";
 
 		//$upload_path = "./uploads/$page_controllers/myfile/";
-		echo $excelFile = $upload_path.$filename;
+		$excelFile = $upload_path.$filename;
 		if(file_exists($excelFile))
 		{
 			echo "working";
