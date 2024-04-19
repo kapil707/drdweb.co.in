764a7ec43823e9d61cea66c1c3d3f00dad9d15db
kapil-asus-new <kapil707sharma@gmail.com>
2024-04-19 17:25:36 +0530

    add

--- application/views/admin/manage_bank_whatsapp/view.php
+++ application/views/admin/manage_bank_whatsapp/view.php
@@ -124,8 +124,7 @@
     </div>
 </div>
 
-
-
+<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 <script>
