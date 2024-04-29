e7a2a8a45ebb9933ea095c3e5df38376c6f9cdf3
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-04-29 13:03:53 +0530

    add

--- application/views/admin/manage_bank_sms/footer2.php
+++ application/views/admin/manage_bank_sms/footer2.php
@@ -1,25 +1,48 @@
+<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 <script>
+var table;
 $(document).ready(function(){
-	let data = [];
-	<?php
-	$i = 1;
-	foreach ($result as $row)
-	{
-		$status = "Inactive";
-		if($row->status==1){
-			$status = "Active";
-		}
-		$passwordstatus = "Inactive";
-		if($row->password!=''){
-			$passwordstatus = "Active";
-		}
-		?>
-		data.push(['<?= ($row->code); ?>', '<?= ($row->altercode); ?>','<?= ($row->name); ?>','<?= ($row->email); ?>','<?= ($row->mobile); ?>','<?= ($status); ?>','<?= ($passwordstatus); ?>','<img src="https://drdweb.co.in/upload_drd_master/chemist_photo/<?= ($row->image); ?>" width=100>','<a href="<?= base_url(); ?>admin/<?php echo $Page_name ?>/edit/<?= ($row->code); ?>">Edit</a>']);
-		<?php
-	}
-	?>
-	$('#example-table').DataTable({
-		data: data,
+
+	var today = new Date();
+    var day = today.getDate();
+    var month = today.getMonth() + 1; // January is 0!
+    var year = today.getFullYear();
+
+    if (day < 10) {
+        day = '0' + day;
+    }
+
+    if (month < 10) {
+        month = '0' + month;
+    }
+
+    var formattedDate = year + '-' + month + '-' + day;
+
+	from_date = to_date = formattedDate;
+
+	table = $('#example-table').DataTable({
+		ajax: {
+		url: 'https://www.drdweb.co.in/upload_sms/api01/get_upload_sms',
+			type: 'POST',
+			data: function(d) {
+				return $.extend({}, d, {
+					from_date: from_date,
+					to_date: to_date
+				});
+			},
+			dataSrc: 'items'
+		},
+		order: [[4, 'desc']],
+		columns: [
+			{ data: 'id', title: 'ID' },
+			{ data: 'sender', title: 'Sender' },
+			{ data: 'message_body', title: 'Message Body' },
+			{ data: 'date', title: 'Date' },
+			{ data: 'time', title: 'Time' },
+			{ data: 'chemist_id', title: 'ChemistID' }
+		],
 		pageLength: 25,
 		responsive: true,
 		dom: '<"html5buttons"B>lTfgitp',
@@ -35,10 +58,51 @@ $(document).ready(function(){
 					$(win.document.body).find('table')
 							.addClass('compact')
 							.css('font-size', 'inherit');
-			}
+				}
 			}
 		]
 	});
-});
+
+	$('#date-range').daterangepicker({
+		opens: 'left', // Date picker position
+		locale: {
+			format: 'DD-MM-YYYY', // Date format
+			separator: ' to ',
+			applyLabel: 'Apply',
+			cancelLabel: 'Cancel',
+			daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr','Sa'],
+			monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+		}
+	});
+
+	$('#date-range').on('apply.daterangepicker', function(ev, picker) {
+		
+        var selectedDates = $('#date-range').val().split(' to ');
+		if (selectedDates.length === 2) {
+			from_date = selectedDates[0].trim();
+			to_date = selectedDates[1].trim();
+
+			from_date 	= data_formet_change(from_date);
+			to_date 	= data_formet_change(to_date);
+		}
+		table.ajax.reload();
+    });
+
+	function data_formet_change(dateValue){
+		var dateParts = dateValue.split('-');
+		if (dateParts.length === 3) {
+			var formattedDate = dateParts[2] + '-' + dateParts[1] + '-' + dateParts[0];
+			return formattedDate;
+		}
+	}
+	//reload_page();
+})
+function reload_page(){
+
+	table.ajax.reload();
+	setInterval(function () {
+		reload_page();
+	}, 120000);
+}
 </script>
 <script src="https://cdn.datatables.net/scroller/2.2.0/js/dataTables.scroller.min.js"></script>
--- application/views/admin/manage_bank_sms/view.php
+++ application/views/admin/manage_bank_sms/view.php
@@ -1,39 +1,110 @@
 <div class="row">
     <div class="col-xs-12" style="margin-bottom:5px;">
-    	<a href="add">
+    	<!-- <a href="add">
             <button type="submit" class="btn btn-info">
                 Add
             </button>
-        </a>
-   	</div>
+        </a> -->
+    </div>
+	<?php
+	$date_range = "";
+	if(isset($_GET["date-range"])){
+		$date_range = $_GET["date-range"];
+	}
+	?>
+	<form method="get" class="mb-5" style="margin-bottom:10px;">
+		<div class="col-md-3">
+			<label for="date-range">Select Date Range:</label>
+		</div>
+		<div class="col-md-3">
+			<input type="text" id="date-range" class="form-control" name="date-range" value="<?php echo $date_range ?>">
+		</div>
+		<div class="col-md-3">
+			<button type="submit" class="btn btn-info submit_button" name="Submit">
+				<i class="ace-icon fa fa-check bigger-110"></i>
+				Submit
+			</button>
+		</div>
+		<div class="col-md-3"></div>
+	</form>
+	<?php 
+
+	$start_date = $end_date = date('d-m-Y');
+	if(isset($_GET["date-range"])){
+		$date_range = $_GET["date-range"];
+
+		// `to` ke aas paas se string ko tukdon mein vibhajit karen
+		$date_parts = explode(" to ", $date_range);
+
+		// Start date aur end date ko extract karen
+		$start_date = $date_parts[0];
+		$end_date 	= $date_parts[1];
+	}
+
+	$start_date = DateTime::createFromFormat('d-m-Y', $start_date);
+	$end_date 	= DateTime::createFromFormat('d-m-Y', $end_date);
+
+	$start_date = $start_date->format('d/m/Y');
+	$end_date 	= $end_date->format('d/m/Y');
+
+	//Created a GET API
+	//http://97.74.82.55:5000/messages?from=07/04/2024&to=07/04/2024
+	//http://172.105.50.148:5000/messages?from=07/04/2024&to=07/04/2024
+	$parmiter = '';
+	$curl = curl_init();
+		
+		curl_setopt_array(
+			$curl,
+			array(
+				CURLOPT_URL =>"http://172.105.50.148:5000/messages?from=$start_date&to=$end_date",
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => '',
+				CURLOPT_MAXREDIRS => 0,
+				CURLOPT_TIMEOUT => 300,
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => 'GET',
+				CURLOPT_POSTFIELDS => $parmiter,
+				CURLOPT_HTTPHEADER => array(
+					'Content-Type: application/json',
+				),
+			)
+		);
+
+		$response = curl_exec($curl);
+		//print_r($response);
+		curl_close($curl);
+
+		$data1 = json_decode($response, true); // Convert JSON string to associative array
+		?>
 	<div class="col-xs-12">
         <div class="table-responsive">
 			<table class="table table-striped table-bordered table-hover" id="example-table">
                 <thead>
                     <tr>
 						<th>
-							Code
+							body
 						</th>
 						<th>
-							Altercode
+							date
 						</th>
 						<th>
-                        	Name
+							extracted_text
                         </th>
 						<th>
-                        	Email 
+							from_number 
 						</th>
 						<th> 
-                        	Mobile
+							message_id
                         </th>
 						<th>
-                        	Status
+							screenshot_image
                         </th>
 						<th>
-                        	Password Status
+							Price
                         </th>
 						<th>
-                        	Image
+							timestamp
                         </th>
 						<th>
                         	Edit
@@ -41,7 +112,68 @@
                     </tr>
                 </thead>
 				<tbody>
-					
+					<?php /*
+					foreach($result as $row){
+						?>
+						<tr><?php $body = utf8_decode($row->body); ?>
+							<td><?php echo $body; ?></td>
+							<td><?php echo $row->date; ?></td>
+							<td><?php echo $row->extracted_text; ?></td>
+							<td><?php echo $row->from_number; ?></td>
+							<td><?php echo $row->message_id; ?></td>
+							<td><?php echo $row->screenshot_image; ?></td>
+							<td><?php echo $row->timestamp; ?></td>
+							<td><?php echo $row->timestamp; ?></td>
+						</tr>
+						<?php
+					}
+					 */?>
+					 <?php
+					if (isset($data1['messages'])) {
+						foreach ($data1['messages'] as $message) {
+							$body = isset($message['body']) ? $message['body'] : "Body not found";
+
+							$date = isset($message['date']) ? $message['date'] : "Date not found";
+
+							$extracted_text = isset($message['extracted_text']) ? $message['extracted_text'] : "extracted_text not found";
+
+							$from_number = isset($message['from_number']) ? $message['from_number'] : "Date not found";
+							
+							$id = isset($message['id']) ? $message['from_number'] : "id not found";
+
+							$screenshot_image = isset($message['screenshot_image']) ? $message['screenshot_image'] : "screenshot_image not found";
+
+							$timestamp = isset($message['timestamp']) ? $message['timestamp'] : "timestamp not found";
+							?>
+							<tr>
+								<td><?php echo $body; ?></td>
+								<td><?php echo $date; ?></td>
+								<td><?php echo $extracted_text; ?></td>
+								<td><?php echo $from_number; ?></td>
+								<td><?php echo $id; ?></td>
+								<td>
+									<a href="https://api.wassi.chat<?php echo $screenshot_image; ?>?token=531fe5caf0e132bdb6000bf01ed66d8cfb75b53606cc8f6eed32509d99d74752f47f288db155557e" target="_blank">	
+										<img src="https://api.wassi.chat<?php echo $screenshot_image; ?>?token=531fe5caf0e132bdb6000bf01ed66d8cfb75b53606cc8f6eed32509d99d74752f47f288db155557e" width="100px">
+									</a>
+								</td>
+								<td>
+									<?php
+									$pattern = '/\d{1,3}(?:,\d{3})*(?:\.\d+)?/'; // Regex pattern to match the price format
+
+									preg_match_all($pattern, $extracted_text, $matches1);
+
+									$price1 = $matches1[0][0] ?? '';
+
+									echo "Price 1: $price1";
+									?>
+								</td>
+								<td><?php echo $timestamp; ?></td>
+								<td><?php echo $timestamp; ?></td>
+							</tr>
+							<?php
+						}
+					}
+					?>
 				</tbody>
 			</table>
 		</div>
