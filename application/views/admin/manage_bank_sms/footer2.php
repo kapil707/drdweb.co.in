d2d7ddb83da8f00a574ed4cb229fac6c38a2ac2a
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-04-29 13:16:57 +0530

    add

--- application/views/admin/manage_bank_sms/footer2.php
+++ application/views/admin/manage_bank_sms/footer2.php
@@ -1,17 +1,57 @@
-<!-- <script>
+<link href="<?= base_url()?>/assets/css/bootstrap.min.css" rel="stylesheet">
+<link href="<?= base_url()?>/assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css">
+<link href="<?= base_url()?>/assets/css/plugins/dataTables/datatables.min.css" rel="stylesheet">
+
+<script src="<?= base_url()?>/assets/js/jquery-3.1.1.min.js"></script>
+<script src="<?= base_url()?>/assets/js/plugins/dataTables/datatables.min.js"></script>
+
+<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+<script>
+var table;
 $(document).ready(function(){
-	let data = [];
-	<?php
-	// $i = 1;
-	// foreach ($result as $row)
-	// {	
-	// 	?>
-	// 	data.push(['<?= ($row->body); ?>', '<?= ($row->date); ?>','<?= ($row->extracted_text); ?>','<?= ($row->from_number); ?>','<?= ($row->message_id); ?>','<?= ($screenshot_image); ?>','<?= ($timestamp); ?>','<a href="<?= base_url(); ?>admin/<?php echo $Page_name ?>/edit/<?= ($row->code); ?>">Edit</a>']);
-	// 	<?php
-	// }
-	?>
-	$('#example-table').DataTable({
-		data: data,
+
+	var today = new Date();
+    var day = today.getDate();
+    var month = today.getMonth() + 1; // January is 0!
+    var year = today.getFullYear();
+
+    if (day < 10) {
+        day = '0' + day;
+    }
+
+    if (month < 10) {
+        month = '0' + month;
+    }
+
+    var formattedDate = year + '-' + month + '-' + day;
+
+	from_date = to_date = formattedDate;
+
+	table = $('#example-table').DataTable({
+		ajax: {
+		url: 'https://www.drdweb.co.in/upload_sms/api01/get_upload_sms',
+			type: 'POST',
+			data: function(d) {
+				return $.extend({}, d, {
+					from_date: from_date,
+					to_date: to_date
+				});
+			},
+			dataSrc: 'items'
+		},
+		order: [[4, 'desc']],
+		columns: [
+			{ data: 'id', title: 'ID' },
+			{ data: 'sender', title: 'Sender' },
+			{ data: 'message_body', title: 'Message Body' },
+			{ data: 'date', title: 'Date' },
+			{ data: 'time', title: 'Time' },
+			{ data: 'chemist_id', title: 'ChemistID' }
+		],
 		pageLength: 25,
 		responsive: true,
 		dom: '<"html5buttons"B>lTfgitp',
@@ -27,19 +67,11 @@ $(document).ready(function(){
 					$(win.document.body).find('table')
 							.addClass('compact')
 							.css('font-size', 'inherit');
-			}
+				}
 			}
 		]
 	});
-});
-</script>
-<script src="https://cdn.datatables.net/scroller/2.2.0/js/dataTables.scroller.min.js"></script> -->
 
-<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
-<script>
-$(document).ready(function(){
 	$('#date-range').daterangepicker({
 		opens: 'left', // Date picker position
 		locale: {
@@ -51,5 +83,35 @@ $(document).ready(function(){
 			monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 		}
 	});
-});
+
+	$('#date-range').on('apply.daterangepicker', function(ev, picker) {
+		
+        var selectedDates = $('#date-range').val().split(' to ');
+		if (selectedDates.length === 2) {
+			from_date = selectedDates[0].trim();
+			to_date = selectedDates[1].trim();
+
+			from_date 	= data_formet_change(from_date);
+			to_date 	= data_formet_change(to_date);
+		}
+		table.ajax.reload();
+    });
+
+	function data_formet_change(dateValue){
+		var dateParts = dateValue.split('-');
+		if (dateParts.length === 3) {
+			var formattedDate = dateParts[2] + '-' + dateParts[1] + '-' + dateParts[0];
+			return formattedDate;
+		}
+	}
+	//reload_page();
+})
+function reload_page(){
+
+	table.ajax.reload();
+	setInterval(function () {
+		reload_page();
+	}, 120000);
+}
 </script>
+<script src="https://cdn.datatables.net/scroller/2.2.0/js/dataTables.scroller.min.js"></script>
