a53f1268b84f2e9ce51b8e632a92d4483a722d9e
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-04-29 13:11:59 +0530

    add

--- application/views/admin/manage_bank_sms/view.php
+++ application/views/admin/manage_bank_sms/view.php
@@ -27,34 +27,152 @@
 		</div>
 		<div class="col-md-3"></div>
 	</form>
-	<div class="col-sm-12  pt-1 pb-5">
-			<table class="table table-striped table-bordered table-hover dataTables-example" id="example-table">
-				<thead>
-					<tr>
+	<?php 
+
+	$start_date = $end_date = date('d-m-Y');
+	if(isset($_GET["date-range"])){
+		$date_range = $_GET["date-range"];
+
+		// `to` ke aas paas se string ko tukdon mein vibhajit karen
+		$date_parts = explode(" to ", $date_range);
+
+		// Start date aur end date ko extract karen
+		$start_date = $date_parts[0];
+		$end_date 	= $date_parts[1];
+	}
+
+	$start_date = DateTime::createFromFormat('d-m-Y', $start_date);
+	$end_date 	= DateTime::createFromFormat('d-m-Y', $end_date);
+
+	$from_date = $start_date->format('Y-m-d');
+	$to_date   = $end_date->format('Y-m-d');
+
+	$parmiter = "{\"from_date\":\"$from_date\",\"to_date\":\"$to_date\"}";
+	$curl = curl_init();
+		
+		curl_setopt_array(
+			$curl,
+			array(
+				CURLOPT_URL =>"https://www.drdweb.co.in/upload_sms/api01/get_upload_sms",
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => '',
+				CURLOPT_MAXREDIRS => 0,
+				CURLOPT_TIMEOUT => 300,
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => 'GET',
+				CURLOPT_POSTFIELDS => $parmiter,
+				CURLOPT_HTTPHEADER => array(
+					'Content-Type: application/json',
+				),
+			)
+		);
+
+		$response = curl_exec($curl);
+		//print_r($response);
+		curl_close($curl);
+
+		$data1 = json_decode($response, true); // Convert JSON string to associative array
+		?>
+	<div class="col-xs-12">
+        <div class="table-responsive">
+			<table class="table table-striped table-bordered table-hover" id="example-table">
+                <thead>
+                    <tr>
 						<th>
-							Sno.
+							body
 						</th>
 						<th>
-							Sender
+							date
 						</th>
 						<th>
-							Message
-						</th>
+							extracted_text
+                        </th>
 						<th>
-							Date
+							from_number 
 						</th>
+						<th> 
+							message_id
+                        </th>
 						<th>
-							Time
-						</th>
+							screenshot_image
+                        </th>
 						<th>
-							ChemistID
-						</th>
-					</tr>
-				</thead>
+							Price
+                        </th>
+						<th>
+							timestamp
+                        </th>
+						<th>
+                        	Edit
+                        </th>
+                    </tr>
+                </thead>
 				<tbody>
-				
+					<?php /*
+					foreach($result as $row){
+						?>
+						<tr><?php $body = utf8_decode($row->body); ?>
+							<td><?php echo $body; ?></td>
+							<td><?php echo $row->date; ?></td>
+							<td><?php echo $row->extracted_text; ?></td>
+							<td><?php echo $row->from_number; ?></td>
+							<td><?php echo $row->message_id; ?></td>
+							<td><?php echo $row->screenshot_image; ?></td>
+							<td><?php echo $row->timestamp; ?></td>
+							<td><?php echo $row->timestamp; ?></td>
+						</tr>
+						<?php
+					}
+					 */?>
+					 <?php
+					if (isset($data1['messages'])) {
+						foreach ($data1['messages'] as $message) {
+							$body = isset($message['body']) ? $message['body'] : "Body not found";
+
+							$date = isset($message['date']) ? $message['date'] : "Date not found";
+
+							$extracted_text = isset($message['extracted_text']) ? $message['extracted_text'] : "extracted_text not found";
+
+							$from_number = isset($message['from_number']) ? $message['from_number'] : "Date not found";
+							
+							$id = isset($message['id']) ? $message['from_number'] : "id not found";
+
+							$screenshot_image = isset($message['screenshot_image']) ? $message['screenshot_image'] : "screenshot_image not found";
+
+							$timestamp = isset($message['timestamp']) ? $message['timestamp'] : "timestamp not found";
+							?>
+							<tr>
+								<td><?php echo $body; ?></td>
+								<td><?php echo $date; ?></td>
+								<td><?php echo $extracted_text; ?></td>
+								<td><?php echo $from_number; ?></td>
+								<td><?php echo $id; ?></td>
+								<td>
+									<a href="https://api.wassi.chat<?php echo $screenshot_image; ?>?token=531fe5caf0e132bdb6000bf01ed66d8cfb75b53606cc8f6eed32509d99d74752f47f288db155557e" target="_blank">	
+										<img src="https://api.wassi.chat<?php echo $screenshot_image; ?>?token=531fe5caf0e132bdb6000bf01ed66d8cfb75b53606cc8f6eed32509d99d74752f47f288db155557e" width="100px">
+									</a>
+								</td>
+								<td>
+									<?php
+									$pattern = '/\d{1,3}(?:,\d{3})*(?:\.\d+)?/'; // Regex pattern to match the price format
+
+									preg_match_all($pattern, $extracted_text, $matches1);
+
+									$price1 = $matches1[0][0] ?? '';
+
+									echo "Price 1: $price1";
+									?>
+								</td>
+								<td><?php echo $timestamp; ?></td>
+								<td><?php echo $timestamp; ?></td>
+							</tr>
+							<?php
+						}
+					}
+					?>
 				</tbody>
 			</table>
 		</div>
-	</div>
+    </div>
 </div>
