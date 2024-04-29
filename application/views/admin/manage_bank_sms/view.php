24bec6a485b6a70d6c231f7464666ef172f7a41a
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-04-29 13:18:48 +0530

    add

--- application/views/admin/manage_bank_sms/view.php
+++ application/views/admin/manage_bank_sms/view.php
@@ -6,55 +6,44 @@
             </button>
         </a> -->
     </div>
-	<?php
-	$date_range = "";
-	if(isset($_GET["date-range"])){
-		$date_range = $_GET["date-range"];
-	}
-	?>
-	<form method="get" class="mb-5" style="margin-bottom:10px;">
-		<div class="col-md-3">
-			<label for="date-range">Select Date Range:</label>
-		</div>
-		<div class="col-md-3">
-			<input type="text" id="date-range" class="form-control" name="date-range" value="<?php echo $date_range ?>">
-		</div>
-		<div class="col-md-3">
-			<button type="submit" class="btn btn-info submit_button" name="Submit">
-				<i class="ace-icon fa fa-check bigger-110"></i>
-				Submit
-			</button>
-		</div>
-		<div class="col-md-3"></div>
-	</form>
 	<div class="col-xs-12">
-        <div class="table-responsive">
-			<table class="table table-striped table-bordered table-hover" id="example-table">
-                <thead>
-					<tr>
-						<th>
-							Sno.
-						</th>
-						<th>
-							Sender
-						</th>
-						<th>
-							Message
-						</th>
-						<th>
-							Date
-						</th>
-						<th>
-							Time
-						</th>
-						<th>
-							ChemistID
-						</th>
-                    </tr>
-                </thead>
-				
-				</tbody>
-			</table>
+		<div class="row">
+			<div class="col-md-3">
+				<label for="date-range">Select Date Range:</label>
+				<input type="text" id="date-range" class="form-control">
+			</div>
 		</div>
-    </div>
+		<div class="row">
+			<div class="col-sm-12  pt-1 pb-5">
+				<div class="table-responsive">
+					<table class="table table-striped table-bordered table-hover" id="example-table">
+						<thead>
+							<tr>
+								<th>
+									Sno.
+								</th>
+								<th>
+									Sender
+								</th>
+								<th>
+									Message
+								</th>
+								<th>
+									Date
+								</th>
+								<th>
+									Time
+								</th>
+								<th>
+									ChemistID
+								</th>
+							</tr>
+						</thead>
+						
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
