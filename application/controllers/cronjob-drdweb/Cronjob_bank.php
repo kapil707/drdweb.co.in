3cdb13e713665f7a4dfb77acd8a2dea6447f2408
kapil-asus-new <kapil707sharma@gmail.com>
2024-05-01 16:54:55 +0530

    add

--- application/controllers/cronjob-drdweb/Cronjob_bank.php
+++ application/controllers/cronjob-drdweb/Cronjob_bank.php
@@ -168,7 +168,7 @@ class Cronjob_bank extends CI_Controller
 
 			if(!empty($received_from)){
 				$received_from1 = str_replace(' ', '', $received_from);
-				$rr = $this->BankModel->select_query("SELECT * FROM `tbl_bank_chemist` WHERE `string_value` LIKE '%$received_from1%'");
+				$rr = $this->BankModel->select_query("SELECT * FROM `tbl_bank_chemist` WHERE `title` LIKE '%$received_from1%'");
 				$rr = $rr->result();
 				foreach($rr as $tt){
 					$chemist_id = $tt->chemist_id;
