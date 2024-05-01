481fe2a7cecbf576f58752643c96db77c81a23b1
kapil-asus-new <kapil707sharma@gmail.com>
2024-05-01 12:23:44 +0530

    add

--- application/views/admin/manage_bank/add.php
+++ application/views/admin/manage_bank/add.php
@@ -189,6 +189,14 @@
 						echo "<b>find5: ".$received_from."</b>"; // Output: 97926121865@PAYTM SAMEER S O KALLU NA
 					}
 
+					preg_match("/FROM(.*?)CITI0000/", $text, $matches);
+					if (!empty($matches)){
+						$received_from = trim($matches[1]);
+						$received_from = str_replace("'", "", $received_from);
+						$received_from = str_replace(" ", "", $received_from);
+						$received_from = str_replace("\n", "", $received_from);
+						echo "<b>find6: ".$received_from."</b>"; // Output: 97926121865@PAYTM SAMEER S O KALLU NA
+					}
 
 					$chmist_id = "";
 					// if(!empty($received_from)){
