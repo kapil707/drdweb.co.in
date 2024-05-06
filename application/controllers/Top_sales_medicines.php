69f56a25fdbf0e475ecec5b04b8e149ecf0062c6
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-05-06 18:06:26 +0530

    add

--- application/controllers/Top_sales_medicines.php
+++ application/controllers/Top_sales_medicines.php
@@ -10,7 +10,7 @@ class Top_sales_medicines extends CI_Controller {
 	
 	public function index()	{
 		$vdt = date("Y-m-d");
-		$result = $this->InvoiceModel->select_query("select DISTINCT item_name,item_code, COUNT(*) as ct FROM tbl_invoice_item where vdt='$vdt' and image1='' GROUP BY item_name,item_code HAVING COUNT(*) > 1 order by ct desc limit 10");
+		$result = $this->InvoiceModel->select_query("select DISTINCT item_name,item_code, COUNT(*) as ct FROM tbl_invoice_item where vdt='$vdt' and image1='' GROUP BY item_name,item_code HAVING COUNT(*) > 1 order by ct desc limit 100");
 		$data["result"] = $result->result();
 		$this->load->view('top_sales_medicines',$data);
 	}
