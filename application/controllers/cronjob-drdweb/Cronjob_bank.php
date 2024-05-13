2af8a1048fe6337008fbb977faff579651e3c6ce
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-05-13 13:31:58 +0530

    add

--- application/controllers/cronjob-drdweb/Cronjob_bank.php
+++ application/controllers/cronjob-drdweb/Cronjob_bank.php
@@ -305,6 +305,8 @@ class Cronjob_bank extends CI_Controller
 				}
 			}
 
+			$find_chemist_id = str_replace("/",",",$find_chemist_id);
+
 			/************************************************* */
 			$result = $this->find_by_invoice($amount,$start_date,$end_date,$find_chemist_id);
 			$find_invoice_chemist_id = $result["find_invoice_chemist_id"];
