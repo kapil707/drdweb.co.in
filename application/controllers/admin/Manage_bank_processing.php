54e9335f56450c7bb6eacadc28be170964131f44
kapil707 <kapilsharma@kapils-MacBook-Air.local>
2024-05-17 14:15:54 +0530

    add

--- application/controllers/admin/Manage_bank_processing.php
+++ application/controllers/admin/Manage_bank_processing.php
@@ -256,7 +256,9 @@ class Manage_bank_processing extends CI_Controller {
 		$upload_path = "./uploads/$page_controllers/photo/";
 		
 		$query = $this->BankModel->select_query("SELECT upi_no, GROUP_CONCAT(id SEPARATOR ', ') AS id, 
-		GROUP_CONCAT(find_by SEPARATOR ', ') AS find_by, 
+		GROUP_CONCAT(find_by SEPARATOR ', ') AS find_by,
+		GROUP_CONCAT(whatsapp_body SEPARATOR ', ') AS whatsapp_body,
+		GROUP_CONCAT(whatsapp_body2 SEPARATOR ', ') AS whatsapp_body2, 
 		GROUP_CONCAT(process_name SEPARATOR ', ') AS process_name, GROUP_CONCAT(date SEPARATOR ', ') AS date,GROUP_CONCAT(time SEPARATOR ', ') AS time, GROUP_CONCAT(process_value SEPARATOR ', ') AS process_value, GROUP_CONCAT(find_chemist_id SEPARATOR ', ') AS find_chemist_id, GROUP_CONCAT(find_invoice_chemist_id SEPARATOR ', ') AS find_invoice_chemist_id, GROUP_CONCAT(done_status SEPARATOR ', ') AS done_status, GROUP_CONCAT(status SEPARATOR ', ') AS status, GROUP_CONCAT(received_from SEPARATOR ', ') AS received_from, GROUP_CONCAT(amount SEPARATOR ', ') AS amount, GROUP_CONCAT(orderid SEPARATOR ', ') AS orderid, GROUP_CONCAT(type SEPARATOR ', ') AS type FROM `tbl_bank_processing` GROUP BY upi_no");
 		$data["result"] = $query->result();
 
