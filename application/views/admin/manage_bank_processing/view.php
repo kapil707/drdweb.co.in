50aeb8acfd8034abdae4736e0d349e2d9bb8cb03
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-05-17 16:34:58 +0530

    add

--- application/views/admin/manage_bank_processing/view.php
+++ application/views/admin/manage_bank_processing/view.php
@@ -186,6 +186,10 @@
 					$whatsapp_body = $entry['statement']['whatsapp_body'];
 					$whatsapp_body2 = $entry['statement']['whatsapp_body2'];
 					$done_status = $entry['statement']['done_status'];
+
+					if(empty($row_id)){
+						$row_id = $entry['sms']['id'];
+					}
 					?>
 					<tr class="tr_css_<?php echo $row_id; ?>" style="<?php echo $tr_style ?>">
 						<td><?php echo $row_id; ?> </td>
