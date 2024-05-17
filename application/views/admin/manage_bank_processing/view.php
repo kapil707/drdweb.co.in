438ffbfda692d9a97b73c967d6bfb45336c42f4d
kapil707 <kapilsharma@kapils-MacBook-Air.local>
2024-05-17 17:06:17 +0530

    add

--- application/views/admin/manage_bank_processing/view.php
+++ application/views/admin/manage_bank_processing/view.php
@@ -276,7 +276,7 @@
 
 							<?= ($received_from); ?>
 
-							<input type="hidden" value="<?php echo $received_from1 ?>" class="text_received_from_<?= ($row_id); ?>">
+							<input type="text" value="<?php echo $received_from1 ?>" class="text_received_from_<?= ($row_id); ?>">
 
 							<input type="text" value="<?php echo $row->find_chemist_id; ?>" class="text_received_from_chemist_id_<?= ($row_id); ?>" style="display:none">
 
