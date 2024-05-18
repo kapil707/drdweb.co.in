8d1186e5d6ddf3ec77dfad748b43583c972ae8c4
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-05-18 11:30:04 +0530

    add

--- application/views/admin/manage_bank_processing/view.php
+++ application/views/admin/manage_bank_processing/view.php
@@ -6,8 +6,31 @@
             </button>
         </a>
    	</div>
+	<?php
+	$date_range = "";
+	if(isset($_GET["date-range"])){
+		$date_range = $_GET["date-range"];
+	}
+	?>
+	<form method="get" class="mb-5" style="margin-bottom:10px;">
+		<div class="col-md-3">
+			<label for="date-range">Select Date Range:</label>
+		</div>
+		<div class="col-md-3">
+			<input type="text" id="date-range" class="form-control" name="date-range" value="<?php echo $date_range ?>">
+		</div>
+		<div class="col-md-3">
+			
+		</div>
+		<div class="col-md-3">
+			<button type="submit" class="btn btn-info submit_button" name="Submit">
+				<i class="ace-icon fa fa-check bigger-110"></i>
+				Submit
+			</button>
+		</div>
+	</form>
 	<div class="col-xs-12">
-	<div class="table-responsive">
+		<div class="table-responsive">
             <table class="table table-striped table-bordered table-hover dataTables-example">
                 <thead>
                     <tr>
