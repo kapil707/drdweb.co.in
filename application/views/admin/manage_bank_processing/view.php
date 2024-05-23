e8aae3a404c0ecb4a210115da4d6d86933ef7a9f
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-05-23 15:34:57 +0530

    add

--- application/views/admin/manage_bank_processing/view.php
+++ application/views/admin/manage_bank_processing/view.php
@@ -328,8 +328,6 @@
 					
 					
 					$done_chemist_id = "";
-					$find_all = "";
-					
 
 
 					
@@ -372,19 +370,19 @@
 					// jab koi be chemist find na ho to
 					/********************************************** */
 					if(empty($find_chemist_id_array[0])){
-						$row_find_by_chemist_id = "N/a";
+						//$row_find_by_chemist_id = "N/a";
 					}
 
 					// jab koi be invoice say chemist find na ho to
 					/********************************************** */
 					if(empty($row_find_by_invoice_chemist_id)){
-						$row_find_by_invoice_chemist_id = "N/a";
+						//$row_find_by_invoice_chemist_id = "N/a";
 					}
 
 					// jab koi be whatapp say chemist find na ho to
 					/********************************************** */
 					if(empty($row_find_by_whatsapp_chemist_id)){
-						$row_find_by_whatsapp_chemist_id = "N/a";
+						//$row_find_by_whatsapp_chemist_id = "N/a";
 					}
 					?>
 					<tr class="tr_css_<?php echo $row_id; ?>" style="<?php echo $tr_style ?>">
