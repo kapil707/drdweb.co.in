3f0ef11e64a8c003c0272501e0cb881d4ed393f0
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-05-29 13:16:05 +0530

    add

--- application/controllers/admin/Manage_bank_statment.php
+++ application/controllers/admin/Manage_bank_statment.php
@@ -127,10 +127,6 @@ class Manage_bank_statment extends CI_Controller {
 						$transaction_description1 = $worksheet->getCell($transaction_description.$row)->getValue();
 						$iban_number1 = $worksheet->getCell($iban_number.$row)->getValue();
 
-						echo $value_date1 = DateTime::createFromFormat('m/d/Y', $value_date1);
-					
-						$value_date1 = $value_date1->format('Y-m-d');
-
 						$dt = array(
 							'account_no'=>$account_no1,
 							'branch_no'=>$branch_no1,
