3072f09aba75a2fe29926f0e77207954a091e5dc
kapilji <kapil707sharma@gmail.com>
2024-06-06 09:09:45 +0530

    add

--- application/models/model-drdweb/BankModel.php
+++ application/models/model-drdweb/BankModel.php
@@ -159,6 +159,9 @@ class BankModel extends CI_Model
 		$start_date = "2024-05-23";
 		$end_date = "2024-05-23";
 
+		echo "SELECT s.*,p.done_chemist_id as chemist_id from tbl_statment as s left JOIN tbl_bank_processing as p on p._id=s.id where p.type='Statment' and s.value_date BETWEEN '$start_date' AND '$end_date'";
+		die();
+
 		$query = $this->BankModel->select_query("SELECT s.*,p.done_chemist_id as chemist_id from tbl_statment as s left JOIN tbl_bank_processing as p on p._id=s.id where p.type='Statment' and s.value_date BETWEEN '$start_date' AND '$end_date'");
 		$result = $query->result();
 		$rowCount = 12;
