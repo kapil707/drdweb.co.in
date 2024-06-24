4985108d980b038496eb1f62a177b69debf2ffda
kapilji <kapil707sharma@gmail.com>
2024-06-24 11:49:08 +0530

    add

--- application/controllers/exe-drdweb/ExeBank.php
+++ application/controllers/exe-drdweb/ExeBank.php
@@ -11,17 +11,20 @@ class ExeBank extends CI_Controller
 	}
 	public function download_sms()
 	{
-		$result = $this->BankModel->select_query("SELECT * FROM `tbl_sms` where download_status=0 order by id asc limit 2");
-		//$result = $result->result();
-		if ($result) {
-			// Fetch the result array
-			$result_array = $result->result_array();
-		
-			// Output the result as JSON
-			echo json_encode($result_array);
-		} else {
-			// Handle the case where the query fails
-			echo json_encode(array("error" => "Failed to fetch records"));
+		if($_POST["last_id"]){
+			$last_id = $_POST["last_id"];
+			$result = $this->BankModel->select_query("SELECT * FROM `tbl_sms` where id>$last_id order by id asc limit 100");
+			//$result = $result->result();
+			if ($result) {
+				// Fetch the result array
+				$result_array = $result->result_array();
+			
+				// Output the result as JSON
+				echo json_encode($result_array);
+			} else {
+				// Handle the case where the query fails
+				echo json_encode(array("error" => "Failed to fetch records"));
+			}
 		}
 	}
 }
