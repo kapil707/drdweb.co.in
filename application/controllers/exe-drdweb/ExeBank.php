58b4203caf8fdbb001c41f7d4d8b96131c1ab795
kapilji <kapil707sharma@gmail.com>
2024-06-24 10:06:52 +0530

    add

--- application/controllers/exe-drdweb/ExeBank.php
+++ application/controllers/exe-drdweb/ExeBank.php
@@ -3,12 +3,19 @@ header('Content-Type: application/json');
 defined('BASEPATH') OR exit('No direct script access allowed');
 class ExeBank extends CI_Controller
 {
+	public function __construct(){
+
+		parent::__construct();
+
+		$this->load->model("model-drdweb/BankModel");
+	}
 	public function download_sms()
 	{
-		$result = $this->db->query("SELECT * FROM `tbl_sms` where download_status=0 order by id asc limit 100")->result();
-		if ($query) {
+		$result = $this->BankModel->select_query("SELECT * FROM `tbl_sms` where download_status=0 order by id asc limit 100");
+		$result = $result->result();
+		if ($result) {
 			// Fetch the result array
-			$result_array = $query->result_array();
+			$result_array = $result->result_array();
 		
 			// Output the result as JSON
 			echo json_encode($result_array);
