99a2501bea88de97dcea8b8903592c21219f3b46
kapilji <kapil707sharma@gmail.com>
2024-08-09 12:39:34 +0530

    add

--- application/controllers/exe-drdweb/ExeDownloadOrder.php
+++ application/controllers/exe-drdweb/ExeDownloadOrder.php
@@ -398,7 +398,7 @@ class ExeDownloadOrder extends CI_Controller
 			$total1 = $row->total1;
 			$this->db->query("update tbl_order set gstvno='$gstvno',download_status=1,download_line='$total' where order_id='$order_id'");
 			/***************only for group message***********************/
-			$group2_message = "TestNew Order No. $order_id download Line Items ($total/$total1)- Easysol No. $gstvno inserted time is : ".date("d-M-y H:i");
+			$group2_message = "TestNew Order No. $order_id download Line Items ($total/$total1) - Easysol No. $gstvno inserted at : ".date("d-M-y H:i");
 			$whatsapp_group2 = $this->Scheme_Model->get_website_data("whatsapp_group2");
 			$this->Message_Model->insert_whatsapp_group_message($whatsapp_group2,$group2_message);
 			/*************************************************************/
