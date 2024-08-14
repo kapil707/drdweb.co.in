4f5d033b247683b00c52f10c3dd6550453074867
kapilji <kapil707sharma@gmail.com>
2024-08-14 12:36:42 +0530

    add

--- application/controllers/exe-drdweb/ExeMedicine.php
+++ application/controllers/exe-drdweb/ExeMedicine.php
@@ -42,7 +42,7 @@ class ExeMedicine extends CI_Controller
 			echo json_encode(["i_code" => $i_code,"status" => "success", "message" => "Data received successfully"]);
 		} else {
 			// Agar data valid nahi hai to error response dena
-			echo json_encode(["status" => "error", "message" => "Invalid data"]);
+			echo json_encode(["i_code" => "error","status" => "error", "message" => "Invalid data"]);
 		}
 	}
 }
