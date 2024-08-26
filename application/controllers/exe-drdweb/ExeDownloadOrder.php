c3c220b3bad74aa0cbe42fb8e0ef999e6f7cac5d
kapilji <kapil707sharma@gmail.com>
2024-08-26 13:30:47 +0530

    add

--- application/controllers/exe-drdweb/ExeDownloadOrder.php
+++ application/controllers/exe-drdweb/ExeDownloadOrder.php
@@ -179,15 +179,15 @@ class ExeDownloadOrder extends CI_Controller
 		}
 	}
 
-	public function update_order_gstvno_test($order_id,$gstvno,$total)
+	public function update_order_gstvno_test($order_id,$gstvno,$insert_total_line,$download_total_line)
 	{
 		if(!empty($order_id) && !empty($gstvno))
 		{
 			$row = $this->db->query("SELECT count(id) as total1 FROM `tbl_order` WHERE `order_id`='$order_id'")->row();
-			$total1 = $row->total1;
+			$total = $row->total1;
 			//$this->db->query("update tbl_order set gstvno='$gstvno',download_status=1,download_line='$total' where order_id='$order_id'");
 			/***************only for group message***********************/
-			$group2_message = "Test Order No. $order_id download Line Items ($total/$total1) - Easysol No. $gstvno inserted at : ".date("d-M-y H:i");
+			$group2_message = "Test Order No. $order_id download Line Items (T:$total/D:$download_total_line/I:$insert_total_line) - Easysol No. $gstvno inserted at : ".date("d-M-y H:i");
 			$whatsapp_group2 = $this->Scheme_Model->get_website_data("whatsapp_group2");
 			$this->Message_Model->insert_whatsapp_group_message($whatsapp_group2,$group2_message);
 
@@ -201,11 +201,14 @@ class ExeDownloadOrder extends CI_Controller
 		}
 	}
 
-	public function update_order_gstvno_error_test($order_id,$gstvno,$total_line,$total)
+	public function update_order_gstvno_error_test($order_id,$gstvno,$insert_total_line,$download_total_line)
 	{
 		if(!empty($order_id) && !empty($gstvno))
 		{
-			$group2_message = "Problem No. $order_id ($total/$total_line) - at : ".date("d-M-y H:i");
+			$row = $this->db->query("SELECT count(id) as total1 FROM `tbl_order` WHERE `order_id`='$order_id'")->row();
+			$total = $row->total1;
+
+			$group2_message = "Problem No. $order_id (T:$total/D:$download_total_line/I:$insert_total_line) - at : ".date("d-M-y H:i");
 			$whatsapp_group2 = $this->Scheme_Model->get_website_data("whatsapp_group2");
 			$this->Message_Model->insert_whatsapp_group_message($whatsapp_group2,$group2_message);
 		}
