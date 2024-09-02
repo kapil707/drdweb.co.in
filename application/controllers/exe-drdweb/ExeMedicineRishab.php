f8702a29ba5059e1661071bed04e54b9f766556f
kapilji <kapil707sharma@gmail.com>
2024-09-02 12:34:07 +0530

    add

--- application/controllers/exe-drdweb/ExeMedicineRishab.php
+++ application/controllers/exe-drdweb/ExeMedicineRishab.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class ExeMedicine extends CI_Controller
+class ExeMedicineRishab extends CI_Controller
 {
 	public function __construct(){
 
