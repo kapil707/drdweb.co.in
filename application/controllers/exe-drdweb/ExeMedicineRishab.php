5b035806543fad0c10131694a70186628740e67a
kapilji <kapil707sharma@gmail.com>
2024-09-02 12:33:36 +0530

    add

---
+++ application/controllers/exe-drdweb/ExeMedicineRishab.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class ExeMedicine extends CI_Controller
+{
+	public function __construct(){
+
+		parent::__construct();
+
+		// Load model
+		//$this->load->model("model-drdweb/InvoiceModel");
+	}
+	public function download_medicines()
+	{
+		$data = $this->db->get('tbl_medicine_new');
+        echo json_encode($data);
+	}
+}
