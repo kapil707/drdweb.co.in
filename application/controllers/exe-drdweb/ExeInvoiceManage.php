de29afe6b426f4647ff45fbf1957b2155b4da89a
kapilji <kapil707sharma@gmail.com>
2024-10-01 13:42:09 +0530

    add

--- application/controllers/exe-drdweb/ExeInvoiceManage.php
+++ application/controllers/exe-drdweb/ExeInvoiceManage.php
@@ -14,71 +14,78 @@ class ExeInvoiceManage extends CI_Controller
 		$isdone = "";
 		
 		$data = json_decode(file_get_contents('php://input'),true);
-		$items = $data["items"];
-		foreach($items as $row)
-		{
-			if (!empty($row["id"])) {
-				
-				$id 			= $row["id"];
-				$dispatchtime 	= $row["dispatchtime"];
-				$vdt 			= $row["vdt"];
-				$vno 			= $row["vno"];
-				$vtype 			= $row["vtype"];
-				$gstvno 		= $row["gstvno"];
-				$pickedby 		= $row["pickedby"];
-				$checkedby 		= $row["checkedby"];
-				$deliverby 		= $row["deliverby"];
-				$amt 			= $row["amt"];
-				$taxamt 		= $row["taxamt"];
-                $acno 			= $row["acno"];
-				$chemist_id 	= $row["chemist_id"];
-				$chemist_name 	= $row["chemist_name"];
-				$chemist_email 	= $row["chemist_email"];
-				$chemist_mobile = $row["chemist_mobile"];
-				$date 			= $row["date"];
-				$update_at 		= $row["update_at"];
-				$status 		= $row["status"];
-				
-				/*******************************************/
-				$where = array('vno'=>$vno,'vdt'=>$vdt);
-				$this->InvoiceModel->delete_fun("tbl_invoice_new",$where);
-				/*******************************************/
-				
-				/*******************************************
-				$where = array('vno'=>$vno,'vdt'=>$vdt);
-				$this->InvoiceModel->delete_fun("tbl_invoice_item",$where);
-				/*******************************************/
-				
-				/*******************************************
-				$where = array('vno'=>$vno,'vdt'=>$vdt);	$this->InvoiceModel->delete_fun("tbl_invoice_item_delete",$where);
-				/*******************************************/
-				
-				$dt = array(
-					'dispatchtime' => $dispatchtime,
-					'vdt' => $vdt,
-					'vno' => $vno,
-					'vtype' => $vtype,
-					'gstvno' => $gstvno,
-					'pickedby' => $pickedby,
-					'checkedby' => $checkedby,
-					'deliverby' => $deliverby,
-					'amt' => $amt,
-					'taxamt' => $taxamt,
-					'acno' => $acno,
-					'chemist_id' => $chemist_id,
-					'chemist_name' => $chemist_name,
-					'chemist_email' => $chemist_email,
-					'chemist_mobile' => $chemist_mobile,
-					'date' => $date,
-					'update_at' => $update_at,
-					'status' => $status,
-				);
-				$this->InvoiceModel->insert_fun("tbl_invoice_new", $dt);
-				$isdone = "yes";
+		// Data ko check karna
+		if ($data && is_array($data)) {
+
+			$id_array = array();
+			foreach ($data as $record) {
+
+				if (!empty($record["id"])) {
+					
+					$id 			= $record["id"];
+					$dispatchtime 	= $record["dispatchtime"];
+					$vdt 			= $record["vdt"];
+					$vno 			= $record["vno"];
+					$vtype 			= $record["vtype"];
+					$gstvno 		= $record["gstvno"];
+					$pickedby 		= $record["pickedby"];
+					$checkedby 		= $record["checkedby"];
+					$deliverby 		= $record["deliverby"];
+					$amt 			= $record["amt"];
+					$taxamt 		= $record["taxamt"];
+					$acno 			= $record["acno"];
+					$chemist_id 	= $record["chemist_id"];
+					$chemist_name 	= $record["chemist_name"];
+					$chemist_email 	= $record["chemist_email"];
+					$chemist_mobile = $record["chemist_mobile"];
+					$date 			= $record["date"];
+					$update_at 		= $record["update_at"];
+					$status 		= $record["status"];
+					
+					/*******************************************/
+					$where = array('vno'=>$vno,'vdt'=>$vdt);
+					$this->InvoiceModel->delete_fun("tbl_invoice_new",$where);
+					/*******************************************/
+					
+					/*******************************************
+					$where = array('vno'=>$vno,'vdt'=>$vdt);
+					$this->InvoiceModel->delete_fun("tbl_invoice_item",$where);
+					/*******************************************/
+					
+					/*******************************************
+					$where = array('vno'=>$vno,'vdt'=>$vdt);	$this->InvoiceModel->delete_fun("tbl_invoice_item_delete",$where);
+					/*******************************************/
+					
+					$dt = array(
+						'dispatchtime' => $dispatchtime,
+						'vdt' => $vdt,
+						'vno' => $vno,
+						'vtype' => $vtype,
+						'gstvno' => $gstvno,
+						'pickedby' => $pickedby,
+						'checkedby' => $checkedby,
+						'deliverby' => $deliverby,
+						'amt' => $amt,
+						'taxamt' => $taxamt,
+						'acno' => $acno,
+						'chemist_id' => $chemist_id,
+						'chemist_name' => $chemist_name,
+						'chemist_email' => $chemist_email,
+						'chemist_mobile' => $chemist_mobile,
+						'date' => $date,
+						'update_at' => $update_at,
+						'status' => $status,
+					);
+					$this->InvoiceModel->insert_fun("tbl_invoice_new", $dt);
+					$isdone = "yes";
+				}
 			}
-		}
-		if ($isdone == "yes") {
-			echo "done";
+			$commaSeparatedString = implode(',', $id_array);
+			// Response dena
+			echo json_encode(["return_values" => $commaSeparatedString,"status" => "success", "message" => "Data received successfully"]);
+		} else {
+			// Agar data valid nahi hai to error response dena
+			echo json_encode(["code" => "error","status" => "error", "message" => "Invalid data"]);
 		}
 	}
 	
