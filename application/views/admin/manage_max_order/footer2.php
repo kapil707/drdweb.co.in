62fa07ebaeaf29d2c71ae95d77eaee55644be0aa
kapil707 <kapilsharma@kapils-MacBook-Air.local>
2024-10-07 10:05:06 +0530

    add

--- application/views/admin/manage_max_order/footer2.php
+++ application/views/admin/manage_max_order/footer2.php
@@ -27,7 +27,7 @@ $(document).ready(function(){
 
 	table = $('#example-table').DataTable({
 		ajax: {
-		url: 'https://www.drdweb.co.in/admin/manage_max_order/view_api?from_date=2&to_date=3',
+		url: '<?php echo base_url(); ?>admin/<?php echo $Page_name ?>/view_api',
 			type: 'POST',
 			data: function(d) {
 				return $.extend({}, d, {
