41e5448ea3cc105cb0adc3adcbb56be1a3f0a588
kapilji <kapil707sharma@gmail.com>
2024-10-15 12:43:45 +0530

    add

--- application/controllers/exe-drdweb/ExeLowStock.php
+++ application/controllers/exe-drdweb/ExeLowStock.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class ExeLowStockManage extends CI_Controller
+class ExeLowStock extends CI_Controller
 {
 	public function __construct(){
 
