25f607bffdd442760bddbbcaef3f6e0887dd574c
kapilji <kapil707sharma@gmail.com>
2024-10-15 15:06:00 +0530

    add

--- application/controllers/cronjob-drdweb/Cronjob_chemist.php
+++ application/controllers/cronjob-drdweb/Cronjob_chemist.php
@@ -22,4 +22,44 @@ class Cronjob_chemist extends CI_Controller
 			$this->db->query("update tbl_android_device_id set logout='1' where user_type='chemist' and chemist_id='$row->altercode'");
 		}
 	}
+
+	public function backup_table($tableName = 'tbl_chemist_other') {
+        // Backup file name with date and time
+        $backupFile = FCPATH . 'backup_' . $tableName . '_' . date('Y-m-d_H-i-s') . '.sql';
+
+        // Open file for writing
+        $fileHandle = fopen($backupFile, 'w');
+        if (!$fileHandle) {
+            echo 'Unable to create backup file';
+            return;
+        }
+
+        // Step 1: Get table structure
+        $structureQuery = $this->db->query("SHOW CREATE TABLE `$tableName`");
+        if ($structureQuery->num_rows() > 0) {
+            $row = $structureQuery->row_array();
+            fwrite($fileHandle, "-- Table structure for `$tableName`\n\n");
+            fwrite($fileHandle, $row['Create Table'] . ";\n\n");
+        }
+
+        // Step 2: Get table data
+        $dataQuery = $this->db->query("SELECT * FROM `$tableName`");
+        if ($dataQuery->num_rows() > 0) {
+            fwrite($fileHandle, "-- Dumping data for `$tableName`\n\n");
+
+            foreach ($dataQuery->result_array() as $row) {
+                $columns = array_keys($row);
+                $values = array_values($row);
+
+                // Prepare SQL insert statement
+                $insertQuery = "INSERT INTO `$tableName` (`" . implode('`, `', $columns) . "`) VALUES ('" . implode("', '", array_map([$this->db, 'escape_str'], $values)) . "');\n";
+                fwrite($fileHandle, $insertQuery);
+            }
+        }
+
+        // Close file
+        fclose($fileHandle);
+
+        echo "Backup successfully created in file: $backupFile";
+    }
 }
