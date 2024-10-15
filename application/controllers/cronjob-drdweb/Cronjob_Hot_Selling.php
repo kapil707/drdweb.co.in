fc35b2f31389399aed3a133a8f81a7977b6409d1
kapilji <kapil707sharma@gmail.com>
2024-10-15 14:39:06 +0530

    add

--- application/controllers/cronjob-drdweb/Cronjob_Hot_Selling.php
+++ application/controllers/cronjob-drdweb/Cronjob_Hot_Selling.php
@@ -8,16 +8,17 @@ class Cronjob_Hot_Selling extends CI_Controller
 
 	public function run_job()
 	{
+		$date = date("Y-m-d");
 		$this->db->query("TRUNCATE TABLE tbl_hot_selling;");
-		$result = $this->db->query("SELECT `itemc`, COUNT(*) AS total_count FROM `tbl_invoice_item` WHERE `date` = '2024-10-15' GROUP BY `itemc` ORDER BY `total_count` DESC LIMIT 25")->result();
+		$result = $this->db->query("SELECT `itemc`, COUNT(*) AS total_count FROM `tbl_invoice_item` WHERE `date` = '$date' GROUP BY `itemc` ORDER BY `total_count` DESC LIMIT 25")->result();
 		foreach($result as $row)
 		{
-			$item_code 	= $row->itemc;
+			$i_code 	= $row->itemc;
 			$total 		= $row->total_count;
 			$datetime 	= date("d-M-Y h:i a");
 			
 			$dt = array(
-				'item_code'=>$item_code,
+				'i_code'=>$i_code,
 				'total'=>$total,
 				'datetime'=>$datetime,
 			);
