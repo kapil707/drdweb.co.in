58e3a0ae93b39418c4026177da7c22314a46655a
kapilji <kapil707sharma@gmail.com>
2024-10-15 15:11:48 +0530

    add

--- application/controllers/cronjob-drdweb/Cronjob_delete_records.php
+++ application/controllers/cronjob-drdweb/Cronjob_delete_records.php
@@ -30,6 +30,7 @@ class Cronjob_delete_records extends CI_Controller
 		$this->db->query("DELETE FROM `tbl_order` WHERE date<='$day60'");
 		$this->db->query("DELETE FROM `tbl_android_notification` WHERE date<='$day60'");
 		$this->db->query("DELETE FROM `tbl_stock_low` WHERE date<='$day15'");
+		$this->db->query("DELETE FROM `tbl_medicine_compare` WHERE date<='$day15'");
 		$this->db->query("DELETE FROM `tbl_delete_import` WHERE date<='$day60'");
 		$this->db->query("DELETE FROM `tbl_android_device_id` WHERE date<='$day60'");
 		
