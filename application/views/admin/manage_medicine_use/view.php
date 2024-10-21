aedf8e53a2bca00fb13b1fde42494beafd296f8e
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-10-21 15:58:30 +0530

    add

--- application/views/admin/manage_medicine_use/view.php
+++ application/views/admin/manage_medicine_use/view.php
@@ -6,19 +6,6 @@
             </button>
         </a>
    	</div>
-	<!-- <form method="post">
-		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-			<div class="form-group nk-datapk-ctm form-elet-mg" id="data_1">
-				<div class="input-group date nk-int-st">
-					<label>Select Date</label>
-					<input type="date" class="form-control" value="<?= $vdt1; ?>" name="vdt">
-				</div>
-			</div>
-		</div>
-		<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 text-right">
-			<button type="submit" name="submit" class="btn btn-success notika-btn-success waves-effect" value="submit">Submit</button>
-		</div>
-	</form> -->
     <div class="col-xs-12">
          <div class="table-responsive">
 			<table id="data-table-basic" class="table table-striped table-bordered table-hover dataTables-example">
