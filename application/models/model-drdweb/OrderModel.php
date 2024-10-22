06b9a1912f8b2ddad2a6217404e035b527a7f52a
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-10-22 12:38:25 +0530

    add

--- application/models/model-drdweb/OrderModel.php
+++ application/models/model-drdweb/OrderModel.php
@@ -4,6 +4,7 @@ class OrderModel extends CI_Model
 {	
     public function __construct(){
 		parent::__construct();
+        $this->load->model("model-drdweb/EmailModel");
     }
     public function run_job(){
 
@@ -139,8 +140,7 @@ class OrderModel extends CI_Model
                 $mail_server = "";
 				$file_name1 = $file_name2 = $file_name3 = "";
 				$file_name_1 = $file_name_2 = $file_name_3 = "";
-                $this->load->model("model-drdweb/EmailModel");
-				$this->EmailModel->insert_email($user_email_id,$subject,$message,$file_name1,$file_name2,$file_name3,$file_name_1,$file_name_2,$file_name_3,$mail_server,$email_function,$email_other_bcc);
+				$this->EmailModel->insert_email_message($user_email_id,$subject,$message,$file_name1,$file_name2,$file_name3,$file_name_1,$file_name_2,$file_name_3,$mail_server,$email_function,$email_other_bcc);
             }
         }
     }
