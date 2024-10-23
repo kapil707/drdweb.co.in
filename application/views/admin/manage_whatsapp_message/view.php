10a1443ab6e309c86ea50257d3273373aeb8fd16
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-10-23 08:35:18 +0530

    add

--- application/views/admin/manage_whatsapp_message/view.php
+++ application/views/admin/manage_whatsapp_message/view.php
@@ -100,8 +100,3 @@ function delete_rec(id)
 	}
 }
 </script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css">
-
-<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
