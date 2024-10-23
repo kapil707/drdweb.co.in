8d4f6c33b1c88d72456a60dc1c8adf39979cdd30
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-10-23 12:30:30 +0530

    add

--- application/views/admin/header_footer/menu.php
+++ application/views/admin/header_footer/menu.php
@@ -78,7 +78,7 @@
 				</ul>
 			</li>
 			<?php } 
-			$menu = $this->db->query("select DISTINCT tbl_permission_settings.page_type,sorting_order from tbl_permission_settings,tbl_permission_page where tbl_permission_settings.page_type=tbl_permission_page.page_type and user_type='$user_type' and (tbl_permission_settings.page_type='Manage_notification_broadcast' or tbl_permission_settings.page_type='manage_notification' or tbl_permission_settings.page_type='manage_email_notification' or tbl_permission_settings.page_type='manage_whatsapp_message' or tbl_permission_settings.page_type='manage_whatsapp_group_message' or tbl_permission_settings.page_type='manage_email' or tbl_permission_settings.page_type='manage_email_cc') GROUP BY tbl_permission_settings.page_type,sorting_order order by sorting_order asc")->result();
+			$menu = $this->db->query("select DISTINCT tbl_permission_settings.page_type,sorting_order from tbl_permission_settings,tbl_permission_page where tbl_permission_settings.page_type=tbl_permission_page.page_type and user_type='$user_type' and (tbl_permission_settings.page_type='manage_notification_broadcast' or tbl_permission_settings.page_type='manage_notification' or tbl_permission_settings.page_type='manage_email_notification' or tbl_permission_settings.page_type='manage_whatsapp_message' or tbl_permission_settings.page_type='manage_whatsapp_group_message' or tbl_permission_settings.page_type='manage_email' or tbl_permission_settings.page_type='manage_email_cc') GROUP BY tbl_permission_settings.page_type,sorting_order order by sorting_order asc")->result();
 			if(!empty($menu)){
 			?>
 			<li <?php foreach($menu as $mymenu){ if($Page_menu==$mymenu->page_type) { ?> class="active" <?php } }?>>
