160c3197a3f48c3c064f2b2a810e39b219efc560
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-10-31 09:08:46 +0530

    add

--- application/controllers/User_profile.php
+++ application/controllers/User_profile.php
@@ -9,19 +9,21 @@ class User_profile extends CI_Controller {
 	{
         $user_image = "";
 		$upload_image = "./user_profile";
+		if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
+			ini_set('upload_max_filesize', '10M');
+			ini_set('post_max_size', '10M');
+			ini_set('max_input_time', 300);
+			ini_set('max_execution_time', 300);
 
-		ini_set('upload_max_filesize', '10M');
-		ini_set('post_max_size', '10M');
-		ini_set('max_input_time', 300);
-		ini_set('max_execution_time', 300);
+			$new_filename = 'user_' . time() . '_' . uniqid() . '.' . pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
 
-		$config['upload_path'] = $upload_image;  // Define the directory where you want to store the uploaded files.
-		$config['allowed_types'] = 'jpg|jpeg|png|gif';  // Restrict to image file types.
-		$config['max_size'] = 0;  // Set to 0 to allow any size.
+			$config['upload_path'] = $upload_image;  // Define the directory where you want to store the uploaded files.
+			$config['allowed_types'] = 'jpg|jpeg|png|gif';  // Restrict to image file types.
+			$config['max_size'] = 0;  // Set to 0 to allow any size.
+			$config['file_name'] = $new_filename;
 
-		$this->load->library('upload', $config);
+			$this->load->library('upload', $config);
 
-		if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {	
 			if (!$this->upload->do_upload('image')) {  // Changed 'upload_image' to 'image'
 				$error = array('error' => $this->upload->display_errors());
 				$status = 0;
