f3fe6f33b87639fd30c424dbba8219dab2b8a2fb
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-10-31 09:05:03 +0530

    add

--- application/controllers/User_profile.php
+++ application/controllers/User_profile.php
@@ -9,34 +9,34 @@ class User_profile extends CI_Controller {
 	{
         $user_image = "";
 		$upload_image = "user_profile";
-		if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
-			ini_set('upload_max_filesize', '10M');
-			ini_set('post_max_size', '10M');
-			ini_set('max_input_time', 300);
-			ini_set('max_execution_time', 300);
-	
-			$config['upload_path'] = $upload_image;  // Define the directory where you want to store the uploaded files.
-			$config['allowed_types'] = '*';  // You may want to restrict allowed file types.
-			$config['max_size'] = 0;  // Set to 0 to allow any size.
 
-			$config['file_name'] = time().$_FILES["image"]['name'];
-	
-			$this->load->library('upload', $config);
-	
+		ini_set('upload_max_filesize', '10M');
+		ini_set('post_max_size', '10M');
+		ini_set('max_input_time', 300);
+		ini_set('max_execution_time', 300);
+
+		$config['upload_path'] = $upload_image;  // Define the directory where you want to store the uploaded files.
+		$config['allowed_types'] = '*';  // You may want to restrict allowed file types.
+		$config['max_size'] = 0;  // Set to 0 to allow any size.
+
+		$this->load->library('upload', $config);
+
+		if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {	
 			if (!$this->upload->do_upload('upload_image')) {
 				$error = array('error' => $this->upload->display_errors());
-				//$this->load->view('upload_form', $error);
-				//print_r($error);
 				$status = 0;
 				$status_message = $error;
 			} else {
 				$data = $this->upload->data();
 				$user_image = ($data['file_name']);
 				//$this->load->view('upload_success', $data);
+				if (strpos($user_image, "temp_image") !== false){
+					//$return_message = "ok ha bhai";
+					$user_image = "";
+				}
+				$status = 1;
+				$status_message = "Uploaded successfully.";
 			}
-
-			$status = 1;
-			$status_message = "Uploaded successfully.";
 		} else {
 			// Invalid file or no file uploaded
 
