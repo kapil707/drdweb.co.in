2f22922f3cb06bdce4d08d084cccdd59fb59e839
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-10-31 09:06:40 +0530

    add

--- application/controllers/User_profile.php
+++ application/controllers/User_profile.php
@@ -16,36 +16,34 @@ class User_profile extends CI_Controller {
 		ini_set('max_execution_time', 300);
 
 		$config['upload_path'] = $upload_image;  // Define the directory where you want to store the uploaded files.
-		$config['allowed_types'] = '*';  // You may want to restrict allowed file types.
+		$config['allowed_types'] = 'jpg|jpeg|png|gif';  // Restrict to image file types.
 		$config['max_size'] = 0;  // Set to 0 to allow any size.
 
 		$this->load->library('upload', $config);
 
 		if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {	
-			if (!$this->upload->do_upload('upload_image')) {
+			if (!$this->upload->do_upload('image')) {  // Changed 'upload_image' to 'image'
 				$error = array('error' => $this->upload->display_errors());
 				$status = 0;
 				$status_message = $error;
 			} else {
 				$data = $this->upload->data();
 				$user_image = ($data['file_name']);
-				//$this->load->view('upload_success', $data);
-				if (strpos($user_image, "temp_image") !== false){
-					//$return_message = "ok ha bhai";
-					$user_image = "";
+
+				if (strpos($user_image, "temp_image") !== false) {
+					$user_image = "";  // Clear image if it contains "temp_image"
 				}
 				$status = 1;
 				$status_message = "Uploaded successfully.";
 			}
 		} else {
 			// Invalid file or no file uploaded
-
 			$status = 0;
 			$status_message = "Invalid file or no file uploaded.";
 		}
 
         $response = array(
-            'success' =>$status,
+            'success' => $status,
             'message' => $status_message,
             'user_image' => $user_image,
         );
