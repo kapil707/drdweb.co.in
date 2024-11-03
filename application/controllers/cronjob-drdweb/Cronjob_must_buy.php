4931393cbc9c9686555cc6cf4115313218e1a1c6
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-03 17:18:05 +0530

    add

--- application/controllers/cronjob-drdweb/Cronjob_must_buy.php
+++ application/controllers/cronjob-drdweb/Cronjob_must_buy.php
@@ -8,12 +8,11 @@ class Cronjob_must_buy extends CI_Controller
 
 	public function run_job()
 	{
-		$date = date("Y-m-d");
 		$this->db->query("delete from tbl_medicine_compare where compare_type='must_buy'");
 
 		$this->db->select('i_code, COUNT(*) as total_count');
         $this->db->from('tbl_cart');
-        $this->db->where('date', $date);
+        $this->db->where('date', date("Y-m-d"));
 		$this->db->where('status', 1);
         $this->db->group_by('i_code');
         $this->db->order_by('total_count', 'DESC');
