b63013f4ebf93d32f2a22550f6eed9b4d35f587a
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-03 16:53:46 +0530

    add

--- application/controllers/cronjob-drdweb/Cronjob_Hot_Selling.php
+++ application/controllers/cronjob-drdweb/Cronjob_Hot_Selling.php
@@ -9,7 +9,9 @@ class Cronjob_Hot_Selling extends CI_Controller
 	public function run_job()
 	{
 		$date = date("Y-m-d");
-		$this->db->query("TRUNCATE TABLE tbl_hot_selling;");
+		$this->db->query("delete from tbl_medicine_compare where compare_type='hot_selling'");
+
+		$this->db->query("TRUNCATE TABLE tbl_hot_selling");
 		$result = $this->db->query("SELECT `itemc`, COUNT(*) AS total_count FROM `tbl_invoice_item` WHERE `date` = '$date' GROUP BY `itemc` ORDER BY `total_count` DESC LIMIT 25")->result();
 		foreach($result as $row)
 		{
@@ -23,6 +25,24 @@ class Cronjob_Hot_Selling extends CI_Controller
 				'datetime'=>$datetime,
 			);
 			$this->Scheme_Model->insert_fun("tbl_hot_selling", $dt);
+
+			/********************************************* */
+			$compare_type = "hot_selling";
+			$compare_now = 0;
+			$compare_before = 0;
+			$insert_time = date('Y-m-d,H:i');
+
+			$dt = array(
+				'i_code' => $i_code,
+				'compare_type' => $compare_type,
+				'compare_now' => $compare_now,
+				'compare_before' => $compare_before,
+				'date' => date('Y-m-d'),
+				'time' => date('H:i:s'),
+				'timestamp' => time(),
+				'insert_time' => $insert_time,
+			);
+			$this->Scheme_Model->insert_fun("tbl_medicine_compare", $dt);
 		}
 		echo "Hot Selling Working";
 	}
