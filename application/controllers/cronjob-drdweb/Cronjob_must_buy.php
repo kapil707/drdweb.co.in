374a18c0ae2c4742dfa27e1589e280b58dc816c2
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-03 17:13:47 +0530

    add

--- application/controllers/cronjob-drdweb/Cronjob_must_buy.php
+++ application/controllers/cronjob-drdweb/Cronjob_must_buy.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Cronjob_Hot_Selling extends CI_Controller
+class Cronjob_must_buy extends CI_Controller
 {
 	public function __construct(){
 		parent::__construct();
@@ -9,33 +9,26 @@ class Cronjob_Hot_Selling extends CI_Controller
 	public function run_job()
 	{
 		$date = date("Y-m-d");
-		$this->db->query("delete from tbl_medicine_compare where compare_type='hot_selling'");
+		$this->db->query("delete from tbl_medicine_compare where compare_type='must_buy'");
 
-		$this->db->select('itemc, COUNT(*) as total_count');
-        $this->db->from('tbl_invoice_item');
+		$this->db->select('i_code, COUNT(*) as total_count');
+        $this->db->from('tbl_cart');
         $this->db->where('date', $date);
-        $this->db->group_by('itemc');
+		$this->db->where('status', 1);
+        $this->db->group_by('i_code');
         $this->db->order_by('total_count', 'DESC');
         $this->db->limit(25);
 
         $query = $this->db->get()->result();
 		foreach($query as $row)
 		{
-			$i_code 	= $row->itemc;
+			$i_code 	= $row->i_code;
 			$total 		= $row->total_count;
-			$datetime 	= date("d-M-Y h:i a");
-			
-			$dt = array(
-				'i_code'=>$i_code,
-				'total'=>$total,
-				'datetime'=>$datetime,
-			);
-			$this->Scheme_Model->insert_fun("tbl_hot_selling", $dt);
 
 			/********************************************* */
-			$compare_type 	= "hot_selling";
-			$compare_now 	= $total;
-			$compare_before = $total;
+			$compare_type = "must_buy";
+			$compare_now = 0;
+			$compare_before = 0;
 
 			$dt = array(
 				'i_code' => $i_code,
