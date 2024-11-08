803b8d7b752e0a6ee9983e043f2e9e91cf7498e6
kapilji <kapil707sharma@gmail.com>
2024-11-08 16:47:42 +0530

    add

--- application/controllers/cronjob-drdweb/Cronjob_email.php
+++ application/controllers/cronjob-drdweb/Cronjob_email.php
@@ -1,16 +1,14 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Cronjob_email_message extends CI_Controller 
+class Cronjob_email extends CI_Controller 
 {
 	public function __construct(){
-
 		parent::__construct();
 		// Load model
 		$this->load->model("model-drdweb/EmailModel");
 	}
 	
-	public function send_email()
-	{
+	public function send_email() {
 		//$this->EmailModel->send_email_message();
 		echo "Send Email Working";
 	}
