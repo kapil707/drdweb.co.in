6c4ab864f4f8b8aa61db0011e46082efa79a720d
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2024-11-21 18:57:04 +0530

    add

--- application/controllers/admin/Manage_medicine_menu.php
+++ application/controllers/admin/Manage_medicine_menu.php
@@ -40,9 +40,9 @@ class Manage_medicine_menu extends CI_Controller {
 		
 		$tbl = $Page_tbl;
 		
-		$data['url_path'] 	= base_url()."uploads/manage_company_division_wise/photo/resize/";
-		$upload_path 		= "./uploads/manage_company_division_wise/photo/main/";
-		$upload_resize 		= "./uploads/manage_company_division_wise/photo/resize/";
+		$data['url_path'] 	= base_url()."uploads/manage_company_division/photo/resize/";
+		$upload_path 		= "./uploads/manage_company_division/photo/main/";
+		$upload_resize 		= "./uploads/manage_company_division/photo/resize/";
 		
 		extract($_POST);
 		if(isset($Submit))
@@ -158,9 +158,9 @@ class Manage_medicine_menu extends CI_Controller {
 		
 		$tbl = $Page_tbl;
 		
-		$data['url_path'] 	= base_url()."uploads/manage_company_division_wise/photo/resize/";
-		$upload_path 		= "./uploads/manage_company_division_wise/photo/main/";
-		$upload_resize 		= "./uploads/manage_company_division_wise/photo/resize/";
+		$data['url_path'] 	= base_url()."uploads/manage_company_division/photo/resize/";
+		$upload_path 		= "./uploads/manage_company_division/photo/main/";
+		$upload_resize 		= "./uploads/manage_company_division/photo/resize/";
 		
 		extract($_POST);
 		if(isset($Submit))
@@ -259,7 +259,7 @@ class Manage_medicine_menu extends CI_Controller {
 			$datetime = date("d-M-y @ H:i:s", $row->timestamp);
 
 			if(!empty($image)) {
-				$image = base_url()."uploads/manage_company_division_wise/photo/resize/".$image;
+				$image = base_url()."uploads/manage_company_division/photo/resize/".$image;
 			}
 			$dt = array(
 				'sr_no' => $sr_no,
