be51bab1e53984434604c6be3f693f3fb12d691a
kapil707 <kapilsharma@kapils-MacBook-Air.local>
2024-11-22 10:48:57 +0530

    add

--- application/views/admin/manage_item/edit.php
+++ application/views/admin/manage_item/edit.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="col-xs-12">
-	    <a href="<?php echo base_url(); ?>admin/<?= $Page_name ?>/view?pg=<?= $_GET["pg"]; ?>#row_<?=$id ?>">
+	    <a href="<?php echo base_url(); ?>admin/<?= $Page_name ?>/view">
 		<button type="button" class="btn btn-w-m btn-info"><< Back</button>
 		</a>
 	</div>
@@ -12,22 +12,29 @@
         { ?>
             <div class="form-group">
            		<div class="col-sm-6">
-                    <div class="col-sm-4 text-right">
+				   <div class="col-sm-4 text-right">
                         <label class="control-label" for="form-field-1">
-                            Select Item
+                            Select Medicine
                         </label>
                     </div>
                     <div class="col-sm-8">
-						<input type="hidden" id="i_code" name="i_code" value="<?= $i_code = $row->i_code?>"/>
+						<input type="text" id="find_medicine_id" name="find_medicine_id" value="<?= $row->item_code?>" />
+
 						<?php 
-						$row1 =  $this->db->query("select item_name,item_code from tbl_medicine where i_code='$i_code'")->row();
+						$medicine_name = "";
+						$row1 = $this->db->query ("select item_name,i_code from tbl_medicine where i_code='$row->item_code'")->row();
+						if(!empty($row1)){
+							$medicine_name = $row1->item_name."($row1->i_code)";
+						}
 						?>
-						<input type="text" class="form-control" id="item_name" name="item_name"tabindex="1" onkeydown="call_search_item()" onkeyup="call_search_item()" placeholder="Select Item" autocomplete="off" value="<?= $row1->item_name?> (<?= $row1->item_code?>)" />
-						<div class="call_search_item_result" style="position: absolute;z-index: 1;background: white;width: 300px;"></div>
+
+						<input type="text" class="form-control" id="medicine_name" name="medicine_name" tabindex="1" placeholder="Enter Medicine" autocomplete="off" value="<?= $medicine_name?>" />
+
+						<div class="find_medicine_result"></div>
                     </div>
                     <div class="help-inline col-sm-12 has-error">
                         <span class="help-block reset middle">
-                            <?= form_error('itemid'); ?>
+                            
                         </span>
                     </div>
                 </div>
@@ -54,7 +61,7 @@
                     </div>
                     <div class="help-inline col-sm-12 has-error">
                         <span class="help-block reset middle">
-                            <?= form_error('category_id'); ?>
+                            
                         </span>
                     </div>
                 </div>
@@ -106,135 +113,3 @@
         <!-- PAGE CONTENT ENDS -->
     </div><!-- /.col -->
 </div><!-- /.row -->
-<script>
-function call_search_item()
-{	
-	item_name = $("#item_name").val();
-	$(".call_search_item_result").html("Loading....");
-	$.ajax({
-	type       : "POST",
-	data       :  {item_name:item_name},
-	url        : "<?= base_url()?>admin/<?= $Page_name?>/call_search_item",
-	cache	   : false,
-	success    : function(data){
-		$(".call_search_item_result").html(data);
-		}
-	});
-}
-function additem(i_code,name)
-{
-	name = atob(name);
-	$("#i_code").val(i_code);
-	$("#item_name").val(name);
-	$(".call_search_item_result").html("");
-}
-</script>
-<script>
-var delete_rec1 = 0;
-function delete_photo(path,type_me)
-{
-	if (confirm('Are you sure Delete?')) { 
-	if(delete_rec1==0)
-	{
-		delete_rec1 = 1;
-		$.ajax({
-			type       : "POST",
-			data       :  { path : path , type_me : type_me ,} ,
-			url        : "<?= base_url()?>admin/<?= $Page_name; ?>/delete_photo",
-			success    : function(data){
-					if(data!="")
-					{
-						$(".img_id_"+type_me).hide();
-						$(".old_"+type_me).val('');
-						java_alert_function("success","Photo Deleted");
-						$('.url_error').html("Photo Deleted");
-						
-						java_alert_function("success","Delete Successfully");
-						$("#imgchange").html('<img src="<?= $url_path ?>default.jpg" class="img-responsive" />');
-					}					
-					else
-					{
-						java_alert_function("error","Something Wrong")
-					}
-					delete_rec1 = 0;
-				}
-			});
-		}
-	}
-}
-
-//setTimeout('url_change()',1000);
-function url_change()
-{
-	name = $(".name1").val();
-	name = name.replace(/&/g,'and');
-	name = name.trim(name).replace(/ /g,'-');
-	name = encodeURI(name).replace(/[!\/\\#,+()$~%.'":*?<>{}]/g,'');
-	$(".url1").html(name)
-	$(".url").val(name)
-	a_href_change(name)
-}
-function a_href_change(name)
-{
-	document.getElementById("url1").href= "<?= base_url(); ?>products/"+name+".html"; 
-}
-
-var error2 = 1;
-function change_url()
-{
-	error2 = 0;
-	disabled_submit_button();
-	$('.url_error').html("");
-	url1 = $('.url').val();
-	
-	name = url1;
-	name = name.replace(/&/g,'and');
-	name = name.trim(name).replace(/ /g,'-');
-	name = encodeURI(name).replace(/[!\/\\#,+()$~%.'":*?<>{}]/g,'');
-	$(".url1").html(name)
-	$(".url").val(name)
-	a_href_change(name)
-	
-	$.ajax({
-	type       : "POST",
-	data       :  { url1 : url1,id : '<?= $row->id; ?>',} ,
-	url        : "<?= base_url()?>admin/<?= $Page_name?>/change_url",
-	success    : function(data){
-			if(data!="")
-			{
-				//java_alert_function("success","Delete Successfully");
-				//$('.product_code_error').html("This Product SKU Already Taken");
-				if(data=="Error")
-				{
-					java_alert_function("error","This Product Url Already Taken")
-					$('.url_error').html("This Product Url Already Taken");
-				}
-				if(data=="ok")
-				{
-					java_alert_function("success","Product Url Ok");
-					$('.url_error').html("Product Url Ok");
-					error2 = 1;
-					disabled_submit_button();
-				}
-			}					
-			else
-			{
-				java_alert_function("error","Something Wrong")
-				$('.url_error').html("Something Wrong");
-			}
-		}
-	});
-}
-
-function disabled_submit_button()
-{
-	if(error2==1)
-	{
-		$(".submit_button").prop('disabled', false);
-	}
-	else
-	{
-		$(".submit_button").prop("disabled", true);
-	}
-}
-</script>
