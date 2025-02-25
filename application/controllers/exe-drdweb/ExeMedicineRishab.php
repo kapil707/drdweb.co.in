33b3d0f4ad517fea28b661ecba4823d7c4d6e809
kapilji <kapil707sharma@gmail.com>
2025-02-25 10:04:16 +0530

    add

--- application/controllers/exe-drdweb/ExeMedicineRishab.php
+++ application/controllers/exe-drdweb/ExeMedicineRishab.php
@@ -16,12 +16,20 @@ class ExeMedicineRishab extends CI_Controller
         echo json_encode($data);*/
 	}
 
-	public function download_medicines_id($id)
+	/*public function download_medicines_id($id)
 	{
 		$default2 = $this->load->database('default2', TRUE);
 
 		$where 	= array('id' => $id);
 		$query 	= $default2->where($where)->get('tbl_medicine');
+		$data 	= $query->result_array();
+		echo json_encode($data);
+	}*/
+
+	public function download_medicines_image_id($id)
+	{
+		$where 	= array('id' => $id);
+		$query 	= $this->db->where($where)->get('tbl_medicine_image');
 		$data 	= $query->result_array();
 		echo json_encode($data);
 	}
