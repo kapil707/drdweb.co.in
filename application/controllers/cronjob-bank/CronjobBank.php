241bcd8dac935c62c2be38b3f122fa34d5aafaf0
kapilji <kapil707sharma@gmail.com>
2025-03-12 10:40:47 +0530

    add

--- application/controllers/cronjob-bank/CronjobBank.php
+++ application/controllers/cronjob-bank/CronjobBank.php
@@ -505,7 +505,8 @@ class CronjobBank extends CI_Controller
 			$text = preg_replace('/TXN.*?REF NO/', ' REF NO', $text);
 			$text = preg_replace('/FOR.*?REF NO/', ' REF NO', $text);
 			$text = preg_replace('/CNRBH.*?REF NO/', ' REF NO', $text);
-			$text = preg_replace('/N 0.*?REF NO/', ' REF NO', $text);
+			$text = preg_replace('/N 06.*?REF NO/', ' REF NO', $text);
+			$text = preg_replace('/N06.*?REF NO/', ' REF NO', $text);
 
 			$text = preg_replace('/HDFCH.*?REF NO/', ' REF NO', $text);
 			$text = preg_replace('/H DFCH.*?REF NO/', ' REF NO', $text);
