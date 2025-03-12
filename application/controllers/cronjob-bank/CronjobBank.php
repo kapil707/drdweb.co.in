8798c7b673c1e9fc5c0f78f51de2f384fa20f0c7
kapilji <kapil707sharma@gmail.com>
2025-03-12 16:20:17 +0530

    add

--- application/controllers/cronjob-bank/CronjobBank.php
+++ application/controllers/cronjob-bank/CronjobBank.php
@@ -806,7 +806,7 @@ class CronjobBank extends CI_Controller
 
 	public function find_upi(){
 
-		$result = $this->BankModel->select_query("SELECT * FROM `tbl_whatsapp_message` where upi_no='' ORDER BY RAND() limit 25");
+		$result = $this->BankModel->select_query("SELECT id,vision_text FROM `tbl_whatsapp_message` where upi_no='' ORDER BY RAND() limit 25");
 		$result = $result->result();
 		foreach($result as $row) {
 
