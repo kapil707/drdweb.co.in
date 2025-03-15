81d358f12d9512d317eeffed649b08f5183b3a28
kapilji <kapil707sharma@gmail.com>
2025-03-15 15:07:51 +0530

    add

--- application/controllers/cronjob-bank/CronjobBank.php
+++ application/controllers/cronjob-bank/CronjobBank.php
@@ -1019,7 +1019,7 @@ class CronjobBank extends CI_Controller
 
 			if(empty($upi_no)){
 				// Regex se Transaction ID extract karna
-				preg_match('/Reference Number\s([A-Z0-9]+)/', $text, $matches);
+				preg_match('/Reference\s*Number\s*([A-Z0-9]+)/i', $text, $matches);
 				if (!empty($matches[1])) {
 					$upi_no = $matches[1];
 					$type = 17;
