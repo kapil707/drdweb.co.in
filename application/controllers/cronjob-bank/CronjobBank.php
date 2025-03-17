dbe0fe4e67dcb28c7fc9cf7f8cc73b2fed34a9f7
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-03-17 12:10:45 +0530

    add

--- application/controllers/cronjob-bank/CronjobBank.php
+++ application/controllers/cronjob-bank/CronjobBank.php
@@ -1145,10 +1145,12 @@ class CronjobBank extends CI_Controller
 				$row1 = $this->BankModel->select_query("SELECT body FROM `tbl_whatsapp_message` WHERE from_number='$from_number' AND FROM_UNIXTIME(timestamp) BETWEEN DATE_SUB('$timestamp', INTERVAL 7 MINUTE) AND DATE_ADD('$timestamp', INTERVAL 7 MINUTE) and body!='' LIMIT 0, 25");
 				$row1 = $row1->row();
 				$text = trim($row1->body);
-				//agar pura naam milay to he next prcess karta ha
-				if (strpos($text, $find_chemist) !== false) {
-					$whatsapp_chemist = $find_chemist;
-				} 
+				if(!empty($text)){
+					//agar pura naam milay to he next prcess karta ha
+					if (strpos($text, $find_chemist) !== false) {
+						$whatsapp_chemist = $find_chemist;
+					} 
+				}
 			}
 
 			$where = array(
