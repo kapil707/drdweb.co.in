5d05aaad79c9b46e1384e7b032323b40d4fc07fd
kapil707 <kapilsharma@kapils-MacBook-Air.local>
2025-03-17 09:56:51 +0530

    add

--- application/controllers/cronjob-bank/CronjobBank.php
+++ application/controllers/cronjob-bank/CronjobBank.php
@@ -1120,7 +1120,7 @@ class CronjobBank extends CI_Controller
 			$timestamp = date('Y-m-d H:i:s', $row->timestamp);
 			//agar body m chemist id nahi aa rahi ha to next say find karta ha yha
 			if($whatsapp_chemist!=$find_chemist){
-				$whatsapp_chemist = "sex";
+				$whatsapp_chemist = "";
 				$type = 0;
 			}
 			if(empty($whatsapp_chemist)){
