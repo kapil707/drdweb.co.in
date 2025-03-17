9cbd22703d80d3dc5b0fdac2b1d0493ebe1404a7
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-03-17 12:37:26 +0530

    add

--- application/controllers/cronjob-bank/CronjobBank.php
+++ application/controllers/cronjob-bank/CronjobBank.php
@@ -1123,6 +1123,11 @@ class CronjobBank extends CI_Controller
 				if($whatsapp_chemist!=$find_chemist){
 					$whatsapp_chemist = "";
 				}
+
+				//agar pura naam milay to he next prcess karta ha
+				if (strpos($whatsapp_chemist, $find_chemist) !== false) {
+					$whatsapp_chemist = $find_chemist;
+				} 
 			}
 
 			if(empty($whatsapp_chemist)){
