e11dd3eb8ff3e53cdec309a7333386c0305c9133
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-03-17 16:41:09 +0530

    add

--- application/controllers/cronjob-bank/CronjobBank.php
+++ application/controllers/cronjob-bank/CronjobBank.php
@@ -1307,7 +1307,7 @@ class CronjobBank extends CI_Controller
 		}
 
 		// jab amount or body me chmeist name match karay to
-		$result = $this->BankModel->select_query("SELECT p.upi_no,wm.upi_no,wm.amount, wm.id as whatsapp_id, wm.vision_text FROM tbl_bank_processing AS p JOIN tbl_whatsapp_message wm ON p.amount = wm.amount and wm.date BETWEEN DATE_SUB(p.date, INTERVAL 1 DAY) AND DATE_ADD(p.date, INTERVAL 1 DAY) and REPLACE(TRIM(wm.body), ' ', '')=REPLACE(TRIM(p.find_chemist), ' ', '') and p.whatsapp_id=''");
+		$result = $this->BankModel->select_query("SELECT p.upi_no,wm.amount, wm.id as whatsapp_id, wm.vision_text FROM tbl_bank_processing AS p JOIN tbl_whatsapp_message wm ON p.amount = wm.amount and wm.date BETWEEN DATE_SUB(p.date, INTERVAL 1 DAY) AND DATE_ADD(p.date, INTERVAL 1 DAY) and REPLACE(TRIM(wm.body), ' ', '')=REPLACE(TRIM(p.find_chemist), ' ', '') and p.whatsapp_id=''");
 		$result = $result->result();
 		foreach($result as $row) {
 
