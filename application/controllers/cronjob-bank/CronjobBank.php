85352e07475accc1544b04448c14cf68b5125754
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-03-18 13:51:02 +0530

    add

--- application/controllers/cronjob-bank/CronjobBank.php
+++ application/controllers/cronjob-bank/CronjobBank.php
@@ -1409,6 +1409,7 @@ class CronjobBank extends CI_Controller
 				$dt = array(
 					'upi_no'=>$upi_no,
 				);
+				print_r($dt);
 				$this->BankModel->edit_fun("tbl_whatsapp_message", $dt,$where);
 			}
 		}
