a773cf0c8ce1071830247aed60db77b19c007537
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-03-20 12:54:28 +0530

    add

--- application/controllers/cronjob-bank/CronjobBank.php
+++ application/controllers/cronjob-bank/CronjobBank.php
@@ -6,6 +6,11 @@ class CronjobBank extends CI_Controller
 
 		parent::__construct();
 		$this->load->model("model-bank/BankModel");
+		$this->load->model("model-bank/BankWhatsAppModel");
+	}
+
+	public function insert_whatsapp(){
+		$this->BankWhatsAppModel->insert_whatsapp();
 	}
 
 	public function testing(){
@@ -38,104 +43,7 @@ class CronjobBank extends CI_Controller
 				}
 			}
 		}
-	}
-
-	public function insert_whatsapp(){
-		echo "insert_whatsapp";
-
-		$start_date = date('d-m-Y', strtotime('-1 day'));
-		$end_date = date('d-m-Y');
-
-		$start_date = DateTime::createFromFormat('d-m-Y', $start_date);
-		$end_date 	= DateTime::createFromFormat('d-m-Y', $end_date);
-
-		$start_date = $start_date->format('d/m/Y');
-		$end_date 	= $end_date->format('d/m/Y');
-
-		$sender_name_place = "Online%20Details";
-
-		//Created a GET API
-		$url = "http://192.46.214.43:5000/get_messages_by_status?start_date=$start_date&end_date=$end_date&group=$sender_name_place&status=true";
-
-		$parmiter = '';
-		$curl = curl_init();
-		
-		curl_setopt_array(
-			$curl,
-			array(
-				CURLOPT_URL =>$url,
-				CURLOPT_RETURNTRANSFER => true,
-				CURLOPT_ENCODING => '',
-				CURLOPT_MAXREDIRS => 0,
-				CURLOPT_TIMEOUT => 300,
-				CURLOPT_FOLLOWLOCATION => true,
-				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-				CURLOPT_CUSTOMREQUEST => 'GET',
-				CURLOPT_POSTFIELDS => $parmiter,
-				CURLOPT_HTTPHEADER => array(
-					'Content-Type: application/json',
-					'Authorization: Bearer THIRTEENWOLVESWENTHUNTINGBUT10CAMEBACK'
-				),
-			)
-		);
-
-		$response = curl_exec($curl);
-		//print_r($response);
-		curl_close($curl);
-
-		$data1 = json_decode($response, true);
-
-		if (isset($data1['messages'])) {
-			foreach ($data1['messages'] as $message) {
-				$message_id = isset($message['message_id']) ? $message['message_id'] : "Body not found";
-				$body = isset($message['body']) ? $message['body'] : "Body not found";
-				$date = isset($message['date']) ? $message['date'] : "Date not found";
-				$extracted_text = isset($message['extracted_text']) ? $message['extracted_text'] : "extracted_text not found";
-				$from_number = isset($message['from_number']) ? $message['from_number'] : "Date not found";
-				$ist_timestamp = isset($message['ist_timestamp']) ? $message['ist_timestamp'] : "timestamp not found";
-				$screenshot_image = isset($message['screenshot_image']) ? $message['screenshot_image'] : "screenshot_image not found";
-				$sender_name_place = isset($message['sender_name_place']) ? $message['sender_name_place'] : "sender_name_place not found";
-				$timestamp = isset($message['timestamp']) ? $message['timestamp'] : "timestamp not found";
-				$vision_text = isset($message['vision_text']) ? $message['vision_text'] : "vision_text not found";				
-
-				//$extracted_text = str_replace("\n", "<br>", $extracted_text);
-				//$vision_text = str_replace("\n", "<br>", $vision_text);
-
-				//$date = date('Y-m-d H:i:s', strtotime($date));
-				// Convert date
-				$date = new DateTime($date);
-				// Format as "YYYY-MM-DD"
-				$date = $date->format("Y-m-d");
-
-				$dt = array(
-					'message_id' => $message_id,
-					'body' => $body,
-					'date' => $date,
-					'extracted_text' => $extracted_text,
-					'from_number' => $from_number,
-					'ist_timestamp' => $ist_timestamp,
-					'screenshot_image' => $screenshot_image,
-					'sender_name_place' => $sender_name_place,
-					'timestamp' => $timestamp,
-					'vision_text' => $vision_text,
-				);
-
-				if (!empty($message_id)) {
-					// Check karo agar record already exist karta hai
-					$existing_record = $this->BankModel->select_row("tbl_whatsapp_message", array('message_id' => $message_id));
-			
-					if ($existing_record) {
-						// Agar record exist karta hai to update karo
-						$where = array('message_id' => $message_id);
-						$this->BankModel->edit_fun("tbl_whatsapp_message", $dt, $where);
-					} else {
-						// Agar record exist nahi karta hai to insert karo
-						$this->BankModel->insert_fun("tbl_whatsapp_message", $dt);
-					}
-				}
-			}
-		}
-	}
+	}	
 	
 	public function bank_processing(){
 		
