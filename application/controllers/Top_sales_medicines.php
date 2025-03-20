6599be175a4e7dd35586062cc02ebda729a749cf
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-03-20 11:12:01 +0530

    add

--- application/controllers/Top_sales_medicines.php
+++ application/controllers/Top_sales_medicines.php
@@ -20,7 +20,7 @@ class Top_sales_medicines extends CI_Controller {
 		$items = "";
 		$i = 1;
 
-		$result = $this->BankModel->select_query("SELECT DISTINCT itemc, ANY_VALUE(tbl_medicine.item_name) AS item_name, COUNT(*) AS ct FROM tbl_invoice_item JOIN tbl_medicine ON tbl_medicine.i_code = tbl_invoice_item.itemc WHERE tbl_invoice_item.date = '$date' AND tbl_medicine.image1 = '' GROUP BY itemc HAVING COUNT(*) > 1 ORDER BY ct DESC LIMIT 100");
+		$result = $this->db->query("SELECT DISTINCT itemc, ANY_VALUE(tbl_medicine.item_name) AS item_name, COUNT(*) AS ct FROM tbl_invoice_item JOIN tbl_medicine ON tbl_medicine.i_code = tbl_invoice_item.itemc WHERE tbl_invoice_item.date = '$date' AND tbl_medicine.image1 = '' GROUP BY itemc HAVING COUNT(*) > 1 ORDER BY ct DESC LIMIT 100");
 		$result = $result->result();
 		foreach($result as $row) {
 
