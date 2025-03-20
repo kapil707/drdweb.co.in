5f51d6664e6d11c6db46f16f2056d2253062ebbd
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-03-20 11:11:09 +0530

    add

--- application/controllers/Top_sales_medicines.php
+++ application/controllers/Top_sales_medicines.php
@@ -7,9 +7,59 @@ class Top_sales_medicines extends CI_Controller {
 	}
 	
 	public function index()	{
-		$date = date("Y-m-d");
+		/*$date = date("Y-m-d");
 		$result = $this->db->query("SELECT DISTINCT itemc, ANY_VALUE(tbl_medicine.item_name) AS item_name, COUNT(*) AS ct FROM tbl_invoice_item JOIN tbl_medicine ON tbl_medicine.i_code = tbl_invoice_item.itemc WHERE tbl_invoice_item.date = '$date' AND tbl_medicine.image1 = '' GROUP BY itemc HAVING COUNT(*) > 1 ORDER BY ct DESC LIMIT 100");
-		$data["result"] = $result->result();
+		$data["result"] = $result->result();*/
+		$data = "";
 		$this->load->view('top_sales_medicines',$data);
+	}
+
+	public function view_api() {		
+
+		$jsonArray = array();
+		$items = "";
+		$i = 1;
+
+		$result = $this->BankModel->select_query("SELECT DISTINCT itemc, ANY_VALUE(tbl_medicine.item_name) AS item_name, COUNT(*) AS ct FROM tbl_invoice_item JOIN tbl_medicine ON tbl_medicine.i_code = tbl_invoice_item.itemc WHERE tbl_invoice_item.date = '$date' AND tbl_medicine.image1 = '' GROUP BY itemc HAVING COUNT(*) > 1 ORDER BY ct DESC LIMIT 100");
+		$result = $result->result();
+		foreach($result as $row) {
+
+			$sr_no = $i++;
+			$id = "";
+			
+			$item_code = $row->itemc;
+			$item_name = $row->item_name;
+			$item_total= $row->ct;
+			
+			$time = time();
+			$datetime = date("d-M-y @ H:i:s", $time);
+
+			$dt = array(
+				'sr_no' => $sr_no,
+				'id' => $id,
+				'item_code' => $item_code,
+				'item_name' => $item_name,
+				'item_total' => $item_total,
+				'datetime'=>$datetime,
+			);
+			$jsonArray[] = $dt;
+		}
+		if(!empty($jsonArray)){
+			$items = $jsonArray;
+			$response = array(
+				'success' => "1",
+				'message' => 'Data load successfully',
+				'items' => $items,
+			);
+		}else{
+			$response = array(
+				'success' => "0",
+				'message' => '502 error',
+			);
+		}
+		
+        // Send JSON response
+        header('Content-Type: application/json');
+        echo json_encode($response);
 	}
 }
