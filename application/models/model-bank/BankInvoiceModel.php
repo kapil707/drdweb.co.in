49ef7c343b69b29e9ccce351f80c6376373a6539
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-03-21 16:40:04 +0530

    add

--- application/models/model-bank/BankInvoiceModel.php
+++ application/models/model-bank/BankInvoiceModel.php
@@ -9,11 +9,21 @@ class BankInvoiceModel extends CI_Model
 	}
 
 	public function get_invoice_find_user(){
+
 		$result = $this->BankModel->select_query("select id,find_chemist,amount from tbl_bank_processing where invoice_id='' and find_chemist!='' ORDER BY RAND() limit 100");
 		$result = $result->result();
 		foreach($result as $row){
 			$id 		= $row->id;
 			$chemist_id = $row->find_chemist;
+			$amount 	= $row->amount;
+			$this->invoice_find($id,$chemist_id,$amount);
+		}
+
+		$result = $this->BankModel->select_query("select id,find_chemist,amount from tbl_bank_processing where invoice_id='' and find_chemist!='' and whatsapp_remanded!='' ORDER BY RAND() limit 100");
+		$result = $result->result();
+		foreach($result as $row){
+			$id 		= $row->id;
+			$chemist_id = $row->whatsapp_remanded;
 			$amount 	= $row->amount;
 			$this->invoice_find($id,$chemist_id,$amount);
 		}
@@ -38,6 +48,32 @@ class BankInvoiceModel extends CI_Model
 					'process_status'=>3,
 					'invoice_id'=>$invoice_id,
 					'invoice_chemist'=>$invoice_chemist,
+				);
+				print_r($dt);
+				$this->BankModel->edit_fun("tbl_bank_processing", $dt,$where);
+			}
+		}
+	}
+
+	public function remanded_invoice_find($id,$chemist_id,$amount){
+
+		$start_date = date('Y-m-d', strtotime('-3 day'));
+		$end_date = date('Y-m-d');
+
+		$result = $this->BankModel->select_query("SELECT id FROM `tbl_invoice-bk` WHERE `chemist_id` LIKE '$chemist_id' and  REPLACE(TRIM(amt), '.00', '')='$amount' and date BETWEEN '$start_date' and '$end_date'");
+		$result = $result->result();
+		foreach($result as $row) {
+			if($row->id){
+				$invoice_id = $row->id;
+				$invoice_remanded = $chemist_id;
+
+				$where = array(
+					'id' => $id,
+				);
+				$dt = array(
+					'process_status'=>3,
+					'invoice_id'=>$invoice_id,
+					'invoice_remanded'=>$invoice_remanded,
 				);
 				print_r($dt);
 				$this->BankModel->edit_fun("tbl_bank_processing", $dt,$where);
