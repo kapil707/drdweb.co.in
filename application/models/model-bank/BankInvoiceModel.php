4d67d76ea0310c59ebce7b31898c4319cd8db3aa
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-03-21 16:30:35 +0530

    add

--- application/models/model-bank/BankInvoiceModel.php
+++ application/models/model-bank/BankInvoiceModel.php
@@ -9,25 +9,27 @@ class BankInvoiceModel extends CI_Model
 	}
 
 	public function get_invoice_find_user(){
-		$result = $this->BankModel->select_query("select find_chemist,amount from tbl_bank_processing where invoice_id='' and find_chemist!='' ORDER BY RAND() limit 100");
+		$result = $this->BankModel->select_query("select id,find_chemist,amount from tbl_bank_processing where invoice_id='' and find_chemist!='' ORDER BY RAND() limit 100");
 		$result = $result->result();
 		foreach($result as $row){
+			$id 		= $row->id;
 			$chemist_id = $row->find_chemist;
 			$amount 	= $row->amount;
-			$this->invoice_find($chemist_id,$amount);
+			$this->invoice_find($id,$chemist_id,$amount);
 		}
 	}
 
-	public function invoice_find($chemist_id,$amount){
+	public function invoice_find($id,$chemist_id,$amount){
 
 		$start_date = date('Y-m-d', strtotime('-3 day'));
 		$end_date = date('Y-m-d');
 
-		$result = $this->BankModel->select_query("SELECT * FROM `tbl_invoice-bk` WHERE `chemist_id` LIKE '$chemist_id' and  REPLACE(TRIM(amt), '.00', '')='$amount' and date BETWEEN '$start_date' and '$end_date' ORDER BY RAND() limit 100");
+		$result = $this->BankModel->select_query("SELECT id FROM `tbl_invoice-bk` WHERE `chemist_id` LIKE '$chemist_id' and  REPLACE(TRIM(amt), '.00', '')='$amount' and date BETWEEN '$start_date' and '$end_date' ORDER BY RAND() limit 100");
 		$result = $result->result();
 		foreach($result as $row) {
 			if($row->id){
 				echo $row->id;
+				echo "<br>";
 			}
 		}
 	}
