f30c286c524e815ca00a144901ccc5cd7a31a255
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-03-24 14:13:53 +0530

    add

--- application/views/admin/manage_bank_processing/view.php
+++ application/views/admin/manage_bank_processing/view.php
@@ -371,7 +371,7 @@ $duble_tick = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" cla
 								<?php if($row_whatsapp_text!="") { ?>
 								<div class="col-sm-12">
 									<b onclick="get_whats_message('<?= ($row_id); ?>','<?= ($row_whatsapp_id); ?>','<?= $row_upi_no; ?>')" data-toggle="modal" data-target="#myModal">
-									<style class='<?php if((strtolower($row_whatsapp_chemist)==strtolower($row_chemist_id)) && $row_whatsapp_chemist!="N/a"){ echo "text_find_match"; } ?>'>WhatsApp Text :</style></b>
+									<span class='<?php if((strtolower($row_whatsapp_chemist)==strtolower($row_chemist_id)) && $row_whatsapp_chemist!="N/a"){ echo "text_find_match"; } ?>'>WhatsApp Text :</span></b>
 									<?= ($row_whatsapp_text); ?>
 								</div>
 								<?php } ?>
