5d551525c8cd5e89cf607868557f356e20248ccd
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-03-24 13:49:28 +0530

    add

--- application/views/admin/manage_bank_processing/view.php
+++ application/views/admin/manage_bank_processing/view.php
@@ -303,6 +303,7 @@ $duble_tick = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" sty
 									<div class="td_div">
 										<b>Find  : </b> <?= ($row_from_text_find_match); ?>
 										<?php 
+										echo $row_from_text_find;
 										if(strtolower($row_from_text)==strtolower($row_from_text_find)){
 											echo $duble_tick;
 										}
