37a4f432ce7912314e13eb3d41e1441b989a22fc
kapilji <kapil707sharma@gmail.com>
2025-03-27 19:01:01 +0530

    add

--- application/views/admin/manage_bank_processing/view.php
+++ application/views/admin/manage_bank_processing/view.php
@@ -21,21 +21,21 @@
 	margin: 0px !important;
     padding: 0px !important;
     margin-left: 15px !important;
-	margin-right: -15px !important;
+	margin-right: 15px !important;
     border: 1px solid #676a6c !important;
 }
 .myborder1{
 	margin: 0px !important;
     padding: 0px !important;
 	margin-left: 15px !important;
-	margin-right: -15px !important;
+	margin-right: 15px !important;
     border: 1px solid #676a6c !important;
 }
 .myborder2{
 	margin: 0px !important;
     padding: 0px !important;
 	margin-left: 15px !important;
-	margin-right: -15px !important;
+	margin-right: 15px !important;
     border: 1px solid #676a6c !important;
 }
 .dataTables-example21 td{
