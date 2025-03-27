7c68cd2615b80c20311bc45bb1f3119f1f194d74
kapilji <kapil707sharma@gmail.com>
2025-03-27 18:01:54 +0530

    add

--- application/views/admin/manage_bank_whatsapp/view.php
+++ application/views/admin/manage_bank_whatsapp/view.php
@@ -234,7 +234,7 @@
 										
 										<input type="text" value="<?php echo $chemist_id; ?>" class="text_chemist_<?= ($row_id); ?> pg_text_box" style="float: left !important;display:none;" placeholder="Set Chemist Id">
 									<?php } ?>
-
+									<br>
 									<span class="text_find_match add_chemist_<?= ($row_id); ?>" onclick="add_chemist('<?= ($row_id); ?>')" style="float: left;display:none;"> 
 										Set <i class="fa fa-check" aria-hidden="true"></i>
 									</span>
