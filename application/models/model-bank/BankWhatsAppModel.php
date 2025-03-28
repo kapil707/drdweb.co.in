312f40fe86455160ee11e95e1a318b6c6443b4d7
kapilji <kapil707sharma@gmail.com>
2025-03-28 14:17:51 +0530

    add

--- application/models/model-bank/BankWhatsAppModel.php
+++ application/models/model-bank/BankWhatsAppModel.php
@@ -184,6 +184,14 @@ class BankWhatsAppModel extends CI_Model
 				}
 			}
 
+			if($amount == "0.0"){
+				preg_match('/\b(\d{1,3}(?:,\d{3})*\.\d{2})\b/', $text, $matches);
+				// Check if match is found
+				if (!empty($matches[1])) {
+					$amount = $matches[1];
+				}
+			}
+
 			$type = 0;
 			/************************************************** */
 			// Regular Expression to extract UTR No.
