4543bd1a39d176e77f9b7ddde76f8487165f63fa
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-04-03 14:25:39 +0530

    add

--- application/controllers/Sms.php
+++ application/controllers/Sms.php
@@ -2,10 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Sms extends CI_Controller {
 	public function __construct(){
-
 		parent::__construct();
-
-		$this->load->model("model-drdweb/BankModel");
 	}
 	
 	public function index()	{	
--- application/controllers/upload_sms/Api01.php
+++ application/controllers/upload_sms/Api01.php
@@ -3,10 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Api01 extends CI_Controller {	
 
 	public function __construct(){
-
 		parent::__construct();
-
-		//$this->load->model("model-drdweb/BankModel");
 	}
 
 	public function get_upload_sms() {
